751c7af12248e2759f8b00de49f1d8d36d420e45
Hans Pabst <hans.pabst@intel.com>
2015-03-24 12:31:48 +0100

    Introduced initialization routine for the library; may speed up the auto-dispatcher. Removed legacy function from interface. Minor rewording of the interface documentation.

--- samples/smm.cpp
+++ samples/smm.cpp
@@ -164,6 +164,7 @@ int main(int argc, char* argv[])
       std::vector<T> expect(csize);
 #endif
       fprintf(stdout, "m=%i n=%i k=%i ldc=%i size=%i batch=%i memory=%.1f MB\n", m, n, k, ldc, s, t, mbytes);
+      libxs_init();
 
       { // LAPACK/BLAS3 (fallback)
         fprintf(stdout, "LAPACK/BLAS...\n");
