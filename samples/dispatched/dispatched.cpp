8448daa6b0301119be89f245ca0343b133e3c789
Hans Pabst <hans.pabst@intel.com>
2015-04-29 08:26:30 +0200

    Disabled OMP_DYNAMIC.

--- samples/dispatched/dispatched.cpp
+++ samples/dispatched/dispatched.cpp
@@ -108,6 +108,7 @@ int main(int argc, char* argv[])
         fprintf(stdout, "Streaming...\n");
         std::fill_n(c, s * csize, 0);
 #if defined(_OPENMP)
+        omp_set_dynamic(0);
         const double start = omp_get_wtime();
 #       pragma omp parallel for
 #endif
@@ -127,6 +128,7 @@ int main(int argc, char* argv[])
       { // cached
         fprintf(stdout, "Cached...\n");
 #if defined(_OPENMP)
+        omp_set_dynamic(0);
         const double start = omp_get_wtime();
 #       pragma omp parallel for
 #endif
