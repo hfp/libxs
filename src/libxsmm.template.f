566095c0d888b85061422058815adc26b42c925d
Hans Pabst <hans.pabst@intel.com>
2015-11-19 10:17:59 +0100

    Replaced SELECTED_INT_KIND and selecting via integer precision (relying on C types).

--- src/libxsmm.template.f
+++ src/libxsmm.template.f
@@ -53,7 +53,7 @@
 
         ! Integer type impacting the BLAS interface (LP64: 32-bit, and ILP64: 64-bit).
         INTEGER(C_INT), PARAMETER :: LIBXS_BLASINT_KIND =             &
-     &    SELECTED_INT_KIND(9*($ILP64+1))
+     &    MERGE(C_INT, C_LONG_LONG, 0.EQ.LIBXS_ILP64)
 
         ! Parameters representing the GEMM performed by the simplified interface.
         REAL(C_DOUBLE), PARAMETER :: LIBXS_ALPHA = $ALPHA
