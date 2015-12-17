a9779102a9a3edec4230557e46823568943ce8fd
Hans Pabst <hans.pabst@intel.com>
2015-12-17 18:52:46 +0100

    Shrink the size of the code cache to 1/8th.

--- src/libxs_dispatch.c
+++ src/libxs_dispatch.c
@@ -64,9 +64,9 @@
  * can be also much smaller if never requesting many code versions
  */
 #define LIBXS_DISPATCH_CACHESIZE_BASE (LIBXS_MAX_MNK * 2/*SP+DP*/)
-#define LIBXS_DISPATCH_CACHESIZE_FACTOR 2 /* oversize factor of cache size */
+#define LIBXS_DISPATCH_CACHESIZE_FACTOR 0.125 /* scale factor for the cache size */
 /* size arranged to be (pseudo-)prime number (Mersenne) in order to improve cache spread */
-#define LIBXS_DISPATCH_CACHESIZE ((2 << LIBXS_NBITS(LIBXS_DISPATCH_CACHESIZE_BASE * LIBXS_DISPATCH_CACHESIZE_FACTOR)) - 1)
+#define LIBXS_DISPATCH_CACHESIZE LIBXS_MAX(1023, (2 << LIBXS_NBITS(LIBXS_DISPATCH_CACHESIZE_BASE * LIBXS_DISPATCH_CACHESIZE_FACTOR)) - 1)
 /* flag fused into the memory address of a code version in case of collision */
 #define LIBXS_DISPATCH_HASH_COLLISION (1ULL << (8 * sizeof(void*) - 1))
 #define LIBXS_DISPATCH_HASH_SEED 0 /* CRC32 seed */
