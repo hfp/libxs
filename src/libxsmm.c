50cf57d3fa89d176a878233ec64a3022d686d8c3
Hans Pabst <hans.pabst@intel.com>
2016-01-08 16:01:36 +0100

    Fixed parsing the trace filter values.

--- src/libxsmm.c
+++ src/libxsmm.c
@@ -188,21 +188,28 @@ LIBXS_INLINE LIBXS_RETARGETABLE libxs_cache_entry* internal_init(void)
 #endif
     if (0 == result) {
 #if defined(__TRACE)
-      char* env_trace_init = getenv("LIBXS_TRACE");
-      int match[2], filter_threadid = 0, filter_mindepth = 0, filter_maxdepth = -1;
-      sscanf(env_trace_init, "%[^,]", env_trace_init);
-      sscanf(env_trace_init, "%i", filter_threadid);
-      env_trace_init = getenv("LIBXS_TRACE");
-      sscanf(env_trace_init, "%*[^,],%[^,]", env_trace_init);
-      match[0] = sscanf(env_trace_init, "%i", filter_mindepth);
-      env_trace_init = getenv("LIBXS_TRACE");
-      sscanf(env_trace_init, "%*[^,],%*[^,],%s", env_trace_init);
-      match[1] = sscanf(env_trace_init, "%i", filter_maxdepth);
-      i = (0 == filter_threadid && 0 == match[0] && 0 == match[1]) ? EXIT_SUCCESS
-        : libxs_trace_init(filter_threadid - 1, filter_mindepth, filter_maxdepth);
-#else
-      i = EXIT_SUCCESS;
+      const char *const env_trace_init = getenv("LIBXS_TRACE");
+      if (env_trace_init) {
+        int match[] = { 0, 0 }, filter_threadid = 0, filter_mindepth = 0, filter_maxdepth = -1;
+        char buffer[96];
+
+        if (1 == sscanf(env_trace_init, "%[^,],%*[^,],%*s", buffer)) {
+          sscanf(buffer, "%i", &filter_threadid);
+        }
+        if (1 == sscanf(env_trace_init, "%*[^,],%[^,],%*s", buffer)) {
+          match[0] = sscanf(buffer, "%i", &filter_mindepth);
+        }
+        if (1 == sscanf(env_trace_init, "%*[^,],%*[^,],%s", buffer)) {
+          match[1] = sscanf(buffer, "%i", &filter_maxdepth);
+        }
+        i = (0 == filter_threadid && 0 == match[0] && 0 == match[1]) ? EXIT_SUCCESS
+          : libxs_trace_init(filter_threadid - 1, filter_mindepth, filter_maxdepth);
+      }
+      else
 #endif
+      {
+        i = EXIT_SUCCESS;
+      }
       if (EXIT_SUCCESS == i) {
         result = (libxs_cache_entry*)malloc(LIBXS_CACHESIZE * sizeof(libxs_cache_entry));
 
