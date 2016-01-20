244f5d4296bd366349a09a8de85d3388fc80782d
Hans Pabst <hans.pabst@intel.com>
2016-01-20 12:08:51 +0100

    Avoid warning during offload compilation.

--- src/libxs_crc32.c
+++ src/libxs_crc32.c
@@ -421,7 +421,7 @@ LIBXS_EXTERN_C LIBXS_RETARGETABLE unsigned int libxs_crc32(const void* data, uns
 # if !defined(LIBXS_CRC32_FORCEHW)
 #   define LIBXS_CRC32_FORCEHW
 # endif
-#elif (40400 <= (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__))
+#elif (40400 <= (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)) && !defined(__MIC__)
 # pragma GCC push_options
 # pragma GCC target("sse4.2")
 # include <nmmintrin.h>
@@ -444,6 +444,6 @@ LIBXS_EXTERN_C LIBXS_RETARGETABLE unsigned int libxs_crc32_sse42(const void* dat
   LIBXS_CRC32(internal_crc32_u64, internal_crc32_u32, internal_crc32_u16, internal_crc32_u8, data, size, init);
 #endif
 }
-#if !defined(__SSE4_2__) && (40400 <= (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__))
+#if !defined(__SSE4_2__) && (40400 <= (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)) && !defined(__MIC__)
 # pragma GCC pop_options
 #endif
