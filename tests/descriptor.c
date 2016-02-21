54367c8f76c89fbfaa0b1f976f26128bc7e39cda
Hans Pabst <hans.pabst@intel.com>
2016-02-21 17:05:26 +0100

    Also padded descriptor B since the test case is accessing A and B using the A-slot of libxs_gemm_diff*.

--- tests/descriptor.c
+++ tests/descriptor.c
@@ -17,26 +17,29 @@ int main()
     ? (/*snb*/'b' != cpuid[2] ? libxs_gemm_diff_avx2 : libxs_gemm_diff_avx)
     : (0 != has_crc32/*sse4.2*/ ? libxs_gemm_diff_sse : libxs_gemm_diff);
   const int m = 64, n = 239, k = 64, lda = 64, ldb = 240, ldc = 240;
-  union { libxs_gemm_descriptor descriptor; char simd[32]; } a;
-  libxs_gemm_descriptor b;
+  union { libxs_gemm_descriptor descriptor; char simd[32]; } a, b;
 
   LIBXS_GEMM_DESCRIPTOR(a.descriptor, LIBXS_ALIGNMENT, LIBXS_FLAGS,
     LIBXS_LD(m, n), LIBXS_LD(n, m), k,
     LIBXS_LD(lda, ldb), LIBXS_LD(ldb, lda), ldc,
     LIBXS_ALPHA, LIBXS_BETA,
     LIBXS_PREFETCH_NONE);
-
-#if !defined(LIBXS_GEMM_DIFF_MASK_A)
-  for (i = LIBXS_GEMM_DESCRIPTOR_SIZE; i < sizeof(a.simd); ++i) a.simd[i] = 0;
-#endif
-
-  LIBXS_GEMM_DESCRIPTOR(b, LIBXS_ALIGNMENT, LIBXS_FLAGS,
+  LIBXS_GEMM_DESCRIPTOR(b.descriptor, LIBXS_ALIGNMENT, LIBXS_FLAGS,
     LIBXS_LD(m, n), LIBXS_LD(n, m), k,
     LIBXS_LD(lda, ldb), LIBXS_LD(ldb, lda), ldc,
     LIBXS_ALPHA, LIBXS_BETA,
     LIBXS_PREFETCH_BL2_VIA_C);
+#if defined(LIBXS_GEMM_DIFF_MASK_A)
+  for (i = LIBXS_GEMM_DESCRIPTOR_SIZE; i < sizeof(a.simd); ++i) {
+    a.simd[i] = 'a'; b.simd[i] = 'b';
+  }
+#else
+  for (i = LIBXS_GEMM_DESCRIPTOR_SIZE; i < sizeof(a.simd); ++i) {
+    a.simd[i] = b.simd[i] = 0;
+  }
+#endif
 
-  if (0 == libxs_gemm_diff(&a.descriptor, &b)) {
+  if (0 == libxs_gemm_diff(&a.descriptor, &b.descriptor)) {
     fprintf(stderr, "using static code path\n");
     return 1;
   }
@@ -44,11 +47,11 @@ int main()
     fprintf(stderr, "using static code path\n");
     return 2;
   }
-  else if (0 == libxs_gemm_diff(&b, &a.descriptor)) {
+  else if (0 == libxs_gemm_diff(&b.descriptor, &a.descriptor)) {
     fprintf(stderr, "using static code path\n");
     return 3;
   }
-  else if (0 == diff(&a.descriptor, &b)) {
+  else if (0 == diff(&a.descriptor, &b.descriptor)) {
     fprintf(stderr, "using %s code path\n", cpuid
       ? cpuid : (0 != has_crc32 ? "SSE"
       : (0 != is_static ? "static" : "non-AVX")));
@@ -60,7 +63,7 @@ int main()
       : (0 != is_static ? "static" : "non-AVX")));
     return 5;
   }
-  else if (0 == diff(&b, &a.descriptor)) {
+  else if (0 == diff(&b.descriptor, &a.descriptor)) {
     fprintf(stderr, "using %s code path\n", cpuid
       ? cpuid : (0 != has_crc32 ? "SSE"
       : (0 != is_static ? "static" : "non-AVX")));
