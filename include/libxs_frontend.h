40cf5e097a12cb0d75c7c3b38df04ba0ecf7b286
Hans Pabst <hans.pabst@intel.com>
2016-02-25 11:17:41 +0100

    Some notes about the prefetch helper macros.

--- include/libxs_frontend.h
+++ include/libxs_frontend.h
@@ -75,15 +75,16 @@
 #endif
 
 /** Helper macros for eliding prefetch address calculations depending on prefetch scheme. */
-#if 0 != ((LIBXS_PREFETCH) & 2) || 0 != ((LIBXS_PREFETCH) & 4)
+#if 0 != ((LIBXS_PREFETCH) & 2/*AL2*/) || 0 != ((LIBXS_PREFETCH) & 4/*AL2_JPST*/)
 # define LIBXS_PREFETCH_A(EXPR) (EXPR)
 #endif
-#if 0 != ((LIBXS_PREFETCH) & 8)
+#if 0 != ((LIBXS_PREFETCH) & 8/*BL2_VIA_C*/)
 # define LIBXS_PREFETCH_B(EXPR) (EXPR)
 #endif
 #if 0/*no scheme yet using C*/
 # define LIBXS_PREFETCH_C(EXPR) (EXPR)
 #endif
+/** Secondary helper macros derived from the above group. */
 #if defined(LIBXS_PREFETCH_A)
 # define LIBXS_NOPREFETCH_A(EXPR)
 #else
