ce432de3e280ff655f5993a92325ee4c8f9d27a1
Hans Pabst <hans.pabst@intel.com>
2016-03-18 20:32:08 +0100

    Fixed warning about initializer element which is not computable at load time.

--- tests/descriptor.c
+++ tests/descriptor.c
@@ -126,7 +126,10 @@ int main()
   }
 
   { /* testing diff-search */
-    libxs_gemm_descriptor descs[] = { a.descriptor, a.descriptor, b.descriptor, a.descriptor };
+    libxs_gemm_descriptor descs[4];
+    descs[0] = a.descriptor; descs[1] = a.descriptor;
+    descs[2] = b.descriptor; descs[3] = a.descriptor;
+
     if (0 != libxs_gemm_diffn(&a.descriptor, descs, 0/*hint*/,
       sizeof(descs) / sizeof(*descs), LIBXS_GEMM_DESCRIPTOR_SIZE))
     {
