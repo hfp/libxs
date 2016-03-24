cd0a4f766c60253ec272c06d32529a8fd44d0fd8
Hans Pabst <hans.pabst@intel.com>
2016-03-24 13:38:46 +0100

    Allow setting the prefetch strategy (for JIT) via LIBXS_PREFETCH env. var. even if LIBXS_PREFETCH_AUTO is not selected.

--- src/libxsmm.c
+++ src/libxsmm.c
@@ -427,16 +427,18 @@ LIBXS_INLINE LIBXS_RETARGETABLE internal_regentry* internal_init(void)
       }
       if (0 == internal_target_archid) {
         internal_target_arch = libxs_cpuid_x86(&internal_target_archid);
-        assert(0 != internal_target_arch);
+        assert(0 != internal_target_archid);
       }
-      if (0 > LIBXS_PREFETCH) {
+      { /* select prefetch strategy for JIT */
         const char *const env_prefetch = getenv("LIBXS_PREFETCH");
         if (0 == env_prefetch || 0 == *env_prefetch) {
-          assert(0 != internal_target_arch);
-          internal_prefetch = 0 != strcmp("knl", internal_target_archid)
-            ? LIBXS_PREFETCH_NONE : LIBXS_PREFETCH_AL2BL2_VIA_C;
+          if (0 > LIBXS_PREFETCH) { /* permitted by LIBXS_PREFETCH_AUTO */
+            assert(0 != internal_target_archid);
+            internal_prefetch = 0 != strcmp("knl", internal_target_archid)
+              ? LIBXS_PREFETCH_NONE : LIBXS_PREFETCH_AL2BL2_VIA_C;
+          }
         }
-        else { /* user/runtime input */
+        else { /* user input considered even if LIBXS_PREFETCH_AUTO is disabled */
           switch (atoi(env_prefetch)) {
             case 2: internal_prefetch = LIBXS_PREFETCH_SIGONLY; break;
             case 3: internal_prefetch = LIBXS_PREFETCH_BL2_VIA_C; break;
