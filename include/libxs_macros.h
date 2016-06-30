e080bc679109040d3d1a243b32af6ee6d0ce0e13
Hans Pabst <hans.pabst@intel.com>
2016-06-30 10:17:48 +0200

    Make using GCC's assume expression similar to using a pragma (no sequence point should be needed at the end of the expression).

--- include/libxs_macros.h
+++ include/libxs_macros.h
@@ -188,7 +188,7 @@
 # if defined(_MSC_VER)
 #   define LIBXS_ASSUME(EXPRESSION) __assume(EXPRESSION)
 # elif (LIBXS_VERSION3(4, 5, 0) <= LIBXS_VERSION3(__GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__))
-#   define LIBXS_ASSUME(EXPRESSION) do { if (!(EXPRESSION)) __builtin_unreachable(); } while(0)
+#   define LIBXS_ASSUME(EXPRESSION) do { if (!(EXPRESSION)) __builtin_unreachable(); } while(0);
 # else
 #   define LIBXS_ASSUME(EXPRESSION)
 # endif
