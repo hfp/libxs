6a1815ba3f840d2b630593025cc7763202c9552c
Hans Pabst <hans.pabst@intel.com>
2016-06-30 10:25:27 +0200

    Introduced some argument checks (debug build only). Introduced an optimization, which splits the linear access of the inner loop into fixed chunks; helps code optimization/generation wrt loop bounds (unrolling, etc.). Fixed transpose for rectangular matrices (really the index of the linear access was not correct previously).

--- src/libxs_transpose.c
+++ src/libxs_transpose.c
@@ -6,8 +6,16 @@
 #if !defined(NDEBUG)
 # include <assert.h>
 #endif
+#include <stdio.h>
 #if defined(LIBXS_OFFLOAD_TARGET)
 # pragma offload_attribute(pop)
+#endif
+
+#if !defined(LIBXS_TRANSPOSE_CACHESIZE)
+# define LIBXS_TRANSPOSE_CACHESIZE 32768
+#endif
+#if !defined(LIBXS_TRANSPOSE_N)
+# define LIBXS_TRANSPOSE_N 24
 #endif
 
 
@@ -19,56 +27,89 @@ LIBXS_INLINE LIBXS_RETARGETABLE void inernal_transpose_oop(void* out, const void
   const libxs_blasint m = m1 - m0, n = n1 - n0;
   libxs_blasint i, j;
 
-  if (m * n * typesize <= 16384) {
+#if 0 < LIBXS_TRANSPOSE_N
+  if (m * n * typesize <= (LIBXS_TRANSPOSE_CACHESIZE / 2) && m <= LIBXS_TRANSPOSE_N)
+#else
+  if (m * n * typesize <= (LIBXS_TRANSPOSE_CACHESIZE / 2))
+#endif
+  {
     switch(typesize) {
       case 1: {
         const char *const a = (const char*)in;
         char *const b = (char*)out;
-        for (i = m0; i < m1; ++i) {
-          for (j = n0; j < n1; ++j) {
-            b[i*ldo+j] = a[j*ld+i];
+        for (i = n0; i < n1; ++i) {
+#if 0 < LIBXS_TRANSPOSE_N
+          LIBXS_PRAGMA_LOOP_COUNT(LIBXS_TRANSPOSE_N, LIBXS_TRANSPOSE_N, LIBXS_TRANSPOSE_N)
+#endif
+          for (j = 0; j < m; ++j) {
+            const libxs_blasint k = j + m0;
+            b[i*ldo+k/*consecutive*/] = a[k*ld+i/*strided*/];
           }
         }
       } break;
       case 2: {
         const short *const a = (const short*)in;
         short *const b = (short*)out;
-        for (i = m0; i < m1; ++i) {
-          for (j = n0; j < n1; ++j) {
-            b[i*ldo+j] = a[j*ld+i];
+        for (i = n0; i < n1; ++i) {
+#if 0 < LIBXS_TRANSPOSE_N
+          LIBXS_PRAGMA_LOOP_COUNT(LIBXS_TRANSPOSE_N, LIBXS_TRANSPOSE_N, LIBXS_TRANSPOSE_N)
+#endif
+          for (j = 0; j < m; ++j) {
+            const libxs_blasint k = j + m0;
+            b[i*ldo+k/*consecutive*/] = a[k*ld+i/*strided*/];
           }
         }
       } break;
       case 4: {
         const float *const a = (const float*)in;
         float *const b = (float*)out;
-        for (i = m0; i < m1; ++i) {
-          for (j = n0; j < n1; ++j) {
-            b[i*ldo+j] = a[j*ld+i];
+        for (i = n0; i < n1; ++i) {
+#if 0 < LIBXS_TRANSPOSE_N
+          LIBXS_PRAGMA_LOOP_COUNT(LIBXS_TRANSPOSE_N, LIBXS_TRANSPOSE_N, LIBXS_TRANSPOSE_N)
+#endif
+          for (j = 0; j < m; ++j) {
+            const libxs_blasint k = j + m0;
+            b[i*ldo+k/*consecutive*/] = a[k*ld+i/*strided*/];
           }
         }
       } break;
       case 8: {
         const double *const a = (const double*)in;
         double *const b = (double*)out;
-        for (i = m0; i < m1; ++i) {
-          for (j = n0; j < n1; ++j) {
-            b[i*ldo+j] = a[j*ld+i];
+        for (i = n0; i < n1; ++i) {
+#if 0 < LIBXS_TRANSPOSE_N
+          LIBXS_PRAGMA_LOOP_COUNT(LIBXS_TRANSPOSE_N, LIBXS_TRANSPOSE_N, LIBXS_TRANSPOSE_N)
+#endif
+          for (j = 0; j < m; ++j) {
+            const libxs_blasint k = j + m0;
+            b[i*ldo+k/*consecutive*/] = a[k*ld+i/*strided*/];
           }
         }
       } break;
       default: assert(0);
     }
   }
-  else if (m >= n) {
-    const libxs_blasint mi = (m0 + m1) / 2;
-    inernal_transpose_oop(out, in, typesize, m0, mi, n0, n1, ld, ldo);
-    inernal_transpose_oop(out, in, typesize, mi, m1, n0, n1, ld, ldo);
-  }
-  else {
+  else if (n >= m) {
     const libxs_blasint ni = (n0 + n1) / 2;
     inernal_transpose_oop(out, in, typesize, m0, m1, n0, ni, ld, ldo);
     inernal_transpose_oop(out, in, typesize, m0, m1, ni, n1, ld, ldo);
+  }
+  else {
+#if 0 < LIBXS_TRANSPOSE_N
+    if ((2 * LIBXS_TRANSPOSE_N) < m)
+#endif
+    {
+      const libxs_blasint mi = (m0 + m1) / 2;
+      inernal_transpose_oop(out, in, typesize, m0, mi, n0, n1, ld, ldo);
+      inernal_transpose_oop(out, in, typesize, mi, m1, n0, n1, ld, ldo);
+    }
+#if 0 < LIBXS_TRANSPOSE_N
+    else {
+      const libxs_blasint mi = m0 + LIBXS_TRANSPOSE_N;
+      inernal_transpose_oop(out, in, typesize, m0, mi, n0, n1, ld, ldo);
+      if (mi < m1) inernal_transpose_oop(out, in, typesize, mi, m1, n0, n1, ld, ldo);
+    }
+#endif
   }
 }
 
@@ -76,6 +117,17 @@ LIBXS_INLINE LIBXS_RETARGETABLE void inernal_transpose_oop(void* out, const void
 LIBXS_EXTERN_C LIBXS_RETARGETABLE void libxs_transpose_oop(void* out, const void* in, unsigned int typesize,
   libxs_blasint m, libxs_blasint n, libxs_blasint ld, libxs_blasint ldo)
 {
+#if !defined(NDEBUG) /* library code is expected to be mute */
+  if (ld < LIBXS_LD(n, m) && ldo < LIBXS_LD(m, n)) {
+    fprintf(stderr, "LIBXS: the leading dimensions of the transpose are too small!\n");
+  }
+  else if (ld < LIBXS_LD(n, m)) {
+    fprintf(stderr, "LIBXS: the leading dimension of the transpose input is too small!\n");
+  }
+  else if (ldo < LIBXS_LD(m, n)) {
+    fprintf(stderr, "LIBXS: the leading dimension of the transpose output is too small!\n");
+  }
+#endif
   inernal_transpose_oop(out, in, typesize, 0, m, 0, n, ld, ldo);
 }
 
