d329dcef660ddc3d0b49f736bb586ddc02578c0b
Hans Pabst <hans.pabst@intel.com>
2016-07-26 14:58:39 +0200

    Adjusted test case to generate code sequentially, and to test/receive it in a parallel region.

--- tests/threadsafety.c
+++ tests/threadsafety.c
@@ -20,9 +20,6 @@ int main()
     libxs_init();
   }
 
-#if defined(_OPENMP)
-# pragma omp parallel for default(none) private(i) shared(a, b, f)
-#endif
   for (i = 0; i < MAX_NKERNELS; ++i) {
     const libxs_blasint m = 23, n = 23, k = (i / 50) % 23 + 1;
     /* playing ping-pong with fi's cache line is not the subject */
@@ -31,36 +28,46 @@ int main()
       NULL/*flags*/, NULL/*prefetch*/);
   }
 
+#if defined(_OPENMP)
+# pragma omp parallel for default(none) private(i) shared(a, b, f, result)
+#endif
   for (i = 0; i < MAX_NKERNELS; ++i) {
-    float c[23/*m*/*23/*n*/];
-    const libxs_blasint m = 23, n = 23, k = (i / 50) % 23 + 1;
-    const libxs_smmfunction fi = libxs_smmdispatch(m, n, k,
-      NULL/*lda*/, NULL/*ldb*/, NULL/*ldc*/, NULL/*alpha*/, NULL/*beta*/,
-      NULL/*flags*/, NULL/*prefetch*/);
+    if (0 == result) {
+      const libxs_blasint m = 23, n = 23, k = (i / 50) % 23 + 1;
+      float c[23/*m*/*23/*n*/];
 
-    if (NULL != f[i]) {
-      if (fi == f[i]) {
-        LIBXS_MMCALL(f[i], a, b, c, m, n, k);
-      }
-      else if (NULL != fi) {
+      if (NULL != f[i]) {
+        const libxs_smmfunction fi = libxs_smmdispatch(m, n, k,
+          NULL/*lda*/, NULL/*ldb*/, NULL/*ldc*/, NULL/*alpha*/, NULL/*beta*/,
+          NULL/*flags*/, NULL/*prefetch*/);
+
+        if (fi == f[i]) {
+          LIBXS_MMCALL(f[i], a, b, c, m, n, k);
+        }
+        else if (NULL != fi) {
 #if defined(_DEBUG)
-        fprintf(stderr, "Error: the %ix%ix%i-kernel does not match!\n", m, n, k);
+          fprintf(stderr, "Error: the %ix%ix%i-kernel does not match!\n", m, n, k);
 #endif
-        result = i + 2;
-        break;
-      }
-      else { /* did not find previously generated and recorded kernel */
+#if defined(_OPENMP)
+#         pragma omp atomic write
+#endif
+          result = i + 2;
+        }
+        else { /* did not find previously generated and recorded kernel */
 #if defined(_DEBUG)
-        fprintf(stderr, "Error: cannot find %ix%ix%i-kernel!\n", m, n, k);
+          fprintf(stderr, "Error: cannot find %ix%ix%i-kernel!\n", m, n, k);
 #endif
-        result = 1;
-        break;
+#if defined(_OPENMP)
+#         pragma omp atomic write
+#endif
+          result = 1;
+        }
       }
-    }
-    else {
-      libxs_sgemm(NULL/*transa*/, NULL/*transb*/, &m, &n, &k,
-        NULL/*alpha*/, a, NULL/*lda*/, b, NULL/*ldb*/, 
-        NULL/*beta*/, c, NULL/*ldc*/);
+      else {
+        libxs_sgemm(NULL/*transa*/, NULL/*transb*/, &m, &n, &k,
+          NULL/*alpha*/, a, NULL/*lda*/, b, NULL/*ldb*/, 
+          NULL/*beta*/, c, NULL/*ldc*/);
+      }
     }
   }
 
