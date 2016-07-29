bad421f19509d0bdc9ca48b9b632a5aab9c6ac24
Hans Pabst <hans.pabst@intel.com>
2016-07-29 08:35:35 +0200

    Distinct attribute for weak symbol imports.

--- include/libxs_macros.h
+++ include/libxs_macros.h
@@ -242,10 +242,12 @@
 # define LIBXS_VISIBILITY_INTERNAL
 #endif
 
-#if (defined(__GNUC__) || defined(__clang__)) && !defined(__CYGWIN__)
-# define LIBXS_ATTRIBUTE_WEAK LIBXS_ATTRIBUTE(weak)
+#if (defined(__GNUC__) || defined(__clang__))
+# define LIBXS_ATTRIBUTE_WEAK_EXPORT LIBXS_ATTRIBUTE(weak)
+# define LIBXS_ATTRIBUTE_WEAK_IMPORT LIBXS_ATTRIBUTE(weak_import)
 #else
-# define LIBXS_ATTRIBUTE_WEAK
+# define LIBXS_ATTRIBUTE_WEAK_EXPORT
+# define LIBXS_ATTRIBUTE_WEAK_IMPORT
 #endif
 
 #if defined(NDEBUG)
