9c62fed859b7c1ad6238f8439018efca42da1323
Hans Pabst <hans.pabst@intel.com>
2016-09-01 11:01:50 +0200

    Introduced error handling for transpose routines. Call into in-place transpose if the input and output pointers of the out-of-place transpose are equal, and otherwise compatible (leading dimensions).
    Fortran interface: added new prefetch strategy (LIBXS_PREFETCH_CL2, and derived strategy). Code cleanup (C interface; combined prefetch strategy).

--- src/libxsmm.template.f
+++ src/libxsmm.template.f
@@ -89,10 +89,10 @@
 
         ! Enumeration of the available prefetch strategies which can be IORed.
         INTEGER(C_INT), PARAMETER ::                                    &
-          ! No prefetching and no prefetch function signature.
-     &    LIBXS_PREFETCH_NONE       = 0,                              &
           ! Automatically select strategy (frontend).
      &    LIBXS_PREFETCH_AUTO       = -1,                             &
+          ! No prefetching and no prefetch function signature.
+     &    LIBXS_PREFETCH_NONE       = 0,                              &
           ! Only function prefetch signature.
      &    LIBXS_PREFETCH_SIGONLY    = 1,                              &
           ! Prefetch PA using accesses to A.
@@ -103,9 +103,13 @@
      &    LIBXS_PREFETCH_BL2_VIA_C  = 8,                              &
           ! Prefetch A ahead.
      &    LIBXS_PREFETCH_AL2_AHEAD  = 16,                             &
+          ! Prefetch PC using accesses to C.
+     &    LIBXS_PREFETCH_CL2        = 32,                             &
           ! Composed prefetch strategies.
      &    LIBXS_PREFETCH_AL2BL2_VIA_C = IOR(                          &
      &        LIBXS_PREFETCH_BL2_VIA_C, LIBXS_PREFETCH_AL2),        &
+     &    LIBXS_PREFETCH_AL2CL2BL2_VIA_C = IOR(                       &
+     &        LIBXS_PREFETCH_AL2BL2_VIA_C, LIBXS_PREFETCH_CL2),     &
      &    LIBXS_PREFETCH_AL2BL2_VIA_C_JPST = IOR(                     &
      &        LIBXS_PREFETCH_BL2_VIA_C, LIBXS_PREFETCH_AL2_JPST),   &
      &    LIBXS_PREFETCH_AL2BL2_VIA_C_AHEAD = IOR(                    &
@@ -299,34 +303,37 @@
           END SUBROUTINE
 
           ! Transpose a matrix (out-of-place form).
-          PURE SUBROUTINE libxs_otrans(output,                        &
+          PURE FUNCTION libxs_otrans(output,                          &
      &    input, typesize, m, n, ld, ldo) BIND(C)
             IMPORT LIBXS_BLASINT_KIND, C_PTR, C_INT
             INTEGER(LIBXS_BLASINT_KIND), INTENT(IN), VALUE :: ld, ldo
             INTEGER(LIBXS_BLASINT_KIND), INTENT(IN), VALUE :: m, n
             TYPE(C_PTR), INTENT(IN), VALUE :: output, input
             INTEGER(C_INT), INTENT(IN), VALUE :: typesize
-          END SUBROUTINE
+            INTEGER(C_INT) :: libxs_otrans
+          END FUNCTION
 
           ! Transpose a matrix (out-of-place form, single-precision).
-          PURE SUBROUTINE libxs_sotrans(output,                       &
+          FUNCTION libxs_sotrans(output,                              &
      &    input, m, n, ld, ldo) BIND(C)
-            IMPORT LIBXS_BLASINT_KIND, C_FLOAT
+            IMPORT LIBXS_BLASINT_KIND, C_FLOAT, C_INT
             INTEGER(LIBXS_BLASINT_KIND), INTENT(IN), VALUE :: ld, ldo
             INTEGER(LIBXS_BLASINT_KIND), INTENT(IN), VALUE :: m, n
             REAL(C_FLOAT), INTENT(OUT) :: output(ldo,*)
             REAL(C_FLOAT), INTENT(IN) :: input(ld,*)
-          END SUBROUTINE
+            INTEGER(C_INT) :: libxs_sotrans
+          END FUNCTION
 
           ! Transpose a matrix (out-of-place form, double-precision).
-          PURE SUBROUTINE libxs_dotrans(output,                       &
+          FUNCTION libxs_dotrans(output,                              &
      &    input, m, n, ld, ldo) BIND(C)
-            IMPORT LIBXS_BLASINT_KIND, C_DOUBLE
+            IMPORT LIBXS_BLASINT_KIND, C_DOUBLE, C_INT
             INTEGER(LIBXS_BLASINT_KIND), INTENT(IN), VALUE :: ld, ldo
             INTEGER(LIBXS_BLASINT_KIND), INTENT(IN), VALUE :: m, n
             REAL(C_DOUBLE), INTENT(OUT) :: output(ldo,*)
             REAL(C_DOUBLE), INTENT(IN) :: input(ld,*)
-          END SUBROUTINE
+            INTEGER(C_INT) :: libxs_dotrans
+          END FUNCTION
 
           ! Impure function which returns the current clock tick of a
           ! monotonic timer source; uses a platform-specific resolution.
