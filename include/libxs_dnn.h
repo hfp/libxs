4c0e000dc37eb1cc0d521edb78cc0c68759c94c2
Hans Pabst <hans.pabst@intel.com>
2016-10-02 23:21:24 +0200

    Fixed some typos in documentation and code/interface comments.

--- include/libxs_dnn.h
+++ include/libxs_dnn.h
@@ -120,9 +120,9 @@ typedef struct LIBXS_RETARGETABLE libxs_dnn_conv_desc {
   libxs_dnn_conv_algo algo;                  /* convolution algorithm used */
   libxs_dnn_conv_format buffer_format;       /* format which is for buffer buffers */
   libxs_dnn_conv_format filter_format;       /* format which is for filter buffers */
-  libxs_dnn_conv_fuse_op fuse_ops;           /* used ops into convolutoions */
+  libxs_dnn_conv_fuse_op fuse_ops;           /* used ops into convolutions */
   libxs_dnn_conv_option options;             /* additional options */
-  libxs_dnn_datatype datatype;               /* dataytpes use for all buffers */
+  libxs_dnn_datatype datatype;               /* datatypes use for all buffers */
 } libxs_dnn_conv_desc;
 
 /** get string of error code */
