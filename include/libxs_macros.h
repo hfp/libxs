abc50e95c2218f2fd71979c2a66c1562dbfbbfaa
Hans Pabst <hans.pabst@intel.com>
2016-10-04 12:19:43 +0200

    Disable VLA-bypass with GNU Compiler if C++ translation is used i.e., VLAs are disabled.

--- include/libxs_macros.h
+++ include/libxs_macros.h
@@ -265,7 +265,7 @@
  */
 #if !defined(LIBXS_VLA) && !defined(LIBXS_NO_VLA) && ((defined(__STDC_VERSION__) && (199901L/*C99*/ == __STDC_VERSION__ || \
    (!defined(__STDC_NO_VLA__)&& 199901L/*C99*/ < __STDC_VERSION__))) || (defined(__INTEL_COMPILER) && !defined(_WIN32)) || \
-    (defined(__GNUC__) && !defined(__STRICT_ANSI__))/*depends on above C99-check*/)
+    (defined(__GNUC__) && !defined(__STRICT_ANSI__) && !defined(__cplusplus))/*depends on above C99-check*/)
 # define LIBXS_VLA
 #endif
 
