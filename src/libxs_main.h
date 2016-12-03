3f340a52da31e8313ccc60ad91560389a15ea0f7
Hans Pabst <hans.pabst@intel.com>
2016-12-03 04:10:59 -0800

    Merge remote-tracking branch 'upstream/master'

--- src/libxs_main.h
+++ src/libxs_main.h
@@ -237,7 +237,11 @@ LIBXS_API int libxs_xmalloc(void** memory, size_t size, int alignment, int flags
   const void* extra, size_t extra_size);
 LIBXS_API int libxs_xfree(const volatile void* memory);
 
-/** Attribute memory allocation and protect with only the necessary flags; may relocate the given memory. */
+/**
+ * Attribute memory allocation and protect with only the necessary flags.
+ * This procedure is expected to run only one time per buffer, and may
+ * relocate the given memory.
+ */
 LIBXS_API int libxs_malloc_attrib(void** memory, int flags,
   /** If a name is given, an executable buffer will be dumped into a file. */
   const char* name);
