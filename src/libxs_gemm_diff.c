aef896c11b5c766b3d7c0a0f0dcd92ebc3b3090e
Hans Pabst <hans.pabst@intel.com>
2016-12-08 08:59:53 +0100

    Introduced LIBXS_INTRINSICS_NO_PSEUDO for compilers without support for some of the AVX-512 pseudo intrinsics namely reductions/horizontals.

--- src/libxs_gemm_diff.c
+++ src/libxs_gemm_diff.c
@@ -446,7 +446,7 @@ LIBXS_API_DEFINITION LIBXS_INTRINSICS unsigned int libxs_gemm_diffn_avx512(const
   unsigned int hint, unsigned int ndescs, int nbytes)
 {
 #if !defined(LIBXS_INTRINSICS_NONE) && defined(LIBXS_GEMM_DIFF_AVX512) && (LIBXS_X86_AVX512 <= LIBXS_MAX_STATIC_TARGET_ARCH) && \
-  !defined(__clang__) /* Clang misses at least some of the intrinsics used below */
+  !defined(LIBXS_INTRINSICS_NO_PSEUDO) && !defined(__clang__) /* Clang misses at least some of the intrinsics used below */
   assert(/*is pot*/ndescs == (1 << LIBXS_LOG2(ndescs)));
 # if (28 == LIBXS_GEMM_DESCRIPTOR_SIZE)
   assert(32 == nbytes); /* padded descriptor array */
