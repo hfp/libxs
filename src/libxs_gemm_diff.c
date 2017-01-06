d4c3e98e9f0dcfee0fcd037453967cb2c7459307
Hans Pabst <hans.pabst@intel.com>
2017-01-06 13:50:00 +0100

    Code cleanup.

--- src/libxs_gemm_diff.c
+++ src/libxs_gemm_diff.c
@@ -169,7 +169,6 @@ unsigned int libxs_gemm_diff_avx(const libxs_gemm_descriptor* reference, const l
 # if (28 == LIBXS_GEMM_DESCRIPTOR_SIZE)
   {
     const int yes = 0x80000000, no = 0x0;
-    /* GCC: _mm256_set_epi32 (!NDEBUG) causes an illegal instruction on AVX machines */
     const __m256i m256 = _mm256_set_epi32(no, yes, yes, yes, yes, yes, yes, yes);
 #   if defined(LIBXS_GEMM_DIFF_MASK_A) || !defined(LIBXS_GEMM_DIFF_ZERO_PADDED)
     const __m256i a256 = _mm256_castps_si256(_mm256_maskload_ps((const float*)reference, m256));
@@ -326,7 +325,6 @@ unsigned int libxs_gemm_diffn_avx(
     const char *const desc = (const char*)descs;
     const int yes = 0x80000000, no = 0x0;
     unsigned int i;
-    /* GCC: _mm256_set_epi32 (!NDEBUG) causes an illegal instruction on AVX machines */
     const __m256i m256 = _mm256_set_epi32(no, yes, yes, yes, yes, yes, yes, yes);
 #   if defined(LIBXS_GEMM_DIFF_MASK_A) || !defined(LIBXS_GEMM_DIFF_ZERO_PADDED)
     const __m256i a256 = _mm256_castps_si256(_mm256_maskload_ps((const float*)reference, m256));
