811ee7ee10f35d23d3d4b4d78785ba7babcaaca7
Hans Pabst <hans.pabst@intel.com>
2017-01-15 17:41:03 +0100

    Fixed a typo.

--- tests/threadsafety.c
+++ tests/threadsafety.c
@@ -129,7 +129,7 @@ int main(void)
                 result = EXIT_FAILURE;
               }
 #if defined(_DEBUG) || defined(USE_VERBOSE)
-              else if (0 == registry_nstatic) {
+              else if (0 != registry_nstatic) {
                 fprintf(stderr, "Warning: the %ix%ix%i-kernel may not match!\n", m, n, k);
               }
               else {
