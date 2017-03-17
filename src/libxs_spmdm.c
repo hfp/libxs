41c75d90003ca0ed6008c53a970815e6b6c283dc
Hans Pabst <hans.pabst@intel.com>
2017-03-17 16:01:19 +0100

    In preparation of fast scratch memory allocation, use lower level routines to allocate SPMDM's scratch buffer. This is because the life-time of SPMDM's scratch shall not interfere with the exposed scratch memory domain.

--- src/libxs_spmdm.c
+++ src/libxs_spmdm.c
@@ -109,12 +109,16 @@ LIBXS_INLINE LIBXS_RETARGETABLE void internal_spmdm_allocate_csr_a(libxs_spmdm_h
 
   size_t sz_block = ((handle->bm + 1)*sizeof(uint16_t) + (handle->bm)*(handle->bk)*sizeof(uint16_t) + (handle->bm)*(handle->bk)*sizeof(float) + sizeof(libxs_CSR_sparseslice));
   size_t sz_all_blocks = sz_block * handle->mb * handle->kb;
+  char *const memory_block = 0;
 
-  char *const memory_block = (char *)libxs_aligned_scratch(sz_all_blocks, 2097152);
-  if (0 != memory_block) {
+  /* use low-level scratch memory allocation since life-time of this buffer is unknown */
+  if (EXIT_SUCCESS == libxs_xmalloc((void**)&memory_block, sz_all_blocks, 2097152,
+    LIBXS_MALLOC_FLAG_SCRATCH, 0/*extra*/, 0/*extra_size*/))
+  {
     char * memory_head  = memory_block;
     libxs_CSR_sparseslice* libxs_output_csr_a = (libxs_CSR_sparseslice*)(memory_head);
     memory_head += handle->mb * handle->kb * sizeof(libxs_CSR_sparseslice);
+    assert(0 != memory_block/*sanity check*/);
 
     for (kb = 0; kb < k_blocks; kb++) {
       for (mb = 0; mb < m_blocks; mb++) {
@@ -130,6 +134,10 @@ LIBXS_INLINE LIBXS_RETARGETABLE void internal_spmdm_allocate_csr_a(libxs_spmdm_h
     assert(memory_head == (memory_block + sz_all_blocks));
     *libxs_output_csr = libxs_output_csr_a;
   }
+  else if (0 != libxs_verbosity) { /* library code is expected to be mute */
+    fprintf(stderr, "LIBXS: SPMDM CSR scratch memory allocation failed!\n");
+  }
+
   handle->base_ptr_scratch_A = memory_block;
 }
 
@@ -137,11 +145,22 @@ LIBXS_INLINE LIBXS_RETARGETABLE void internal_spmdm_allocate_csr_a(libxs_spmdm_h
 LIBXS_INLINE LIBXS_RETARGETABLE void internal_spmdm_allocate_scratch(libxs_spmdm_handle* handle, int max_threads)
 {
   size_t sz_memory_for_scratch_per_thread = ((handle->bm)*(handle->bn)*sizeof(float) + (handle->bk)*(handle->bn)*sizeof(float)), sz_total_memory;
-  sz_memory_for_scratch_per_thread = (sz_memory_for_scratch_per_thread + 4095)/4096 * 4096;
+  sz_memory_for_scratch_per_thread = LIBXS_UP2(sz_memory_for_scratch_per_thread, 4096);
   sz_total_memory = sz_memory_for_scratch_per_thread * max_threads;
+  handle->base_ptr_scratch_B_scratch_C = 0;
 
-  handle->base_ptr_scratch_B_scratch_C = (char *)libxs_aligned_scratch(sz_total_memory, 2097152);
-  handle->memory_for_scratch_per_thread = (int)sz_memory_for_scratch_per_thread;
+  /* use low-level scratch memory allocation since life-time of this buffer is unknown */
+  if (EXIT_SUCCESS == libxs_xmalloc((void**)&handle->base_ptr_scratch_B_scratch_C, sz_total_memory, 2097152,
+    LIBXS_MALLOC_FLAG_SCRATCH, 0/*extra*/, 0/*extra_size*/))
+  {
+    handle->memory_for_scratch_per_thread = (int)sz_memory_for_scratch_per_thread;
+  }
+  else {
+    if (0 != libxs_verbosity) { /* library code is expected to be mute */
+      fprintf(stderr, "LIBXS: SPMDM scratch memory allocation failed!\n");
+    }
+    handle->memory_for_scratch_per_thread = 0;
+  }
 }
 
 
