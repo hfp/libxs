5065d4f20f52b778a42a2b326cd374d9e19b9235
Hans Pabst <hans.pabst@intel.com>
2017-03-24 08:01:30 +0100

    Avoid rude type cast. Code cleanup.

--- src/libxs_spmdm.c
+++ src/libxs_spmdm.c
@@ -109,16 +109,16 @@ LIBXS_INLINE LIBXS_RETARGETABLE void internal_spmdm_allocate_csr_a(libxs_spmdm_h
 
   size_t sz_block = ((handle->bm + 1)*sizeof(uint16_t) + (handle->bm)*(handle->bk)*sizeof(uint16_t) + (handle->bm)*(handle->bk)*sizeof(float) + sizeof(libxs_CSR_sparseslice));
   size_t sz_all_blocks = sz_block * handle->mb * handle->kb;
-  char *const memory_block = 0;
+  char* memory_block = 0;
 
   /* use low-level scratch memory allocation since life-time of this buffer is unknown */
   if (EXIT_SUCCESS == libxs_xmalloc((void**)&memory_block, sz_all_blocks, 2097152,
     LIBXS_MALLOC_FLAG_SCRATCH, 0/*extra*/, 0/*extra_size*/))
   {
-    char * memory_head  = memory_block;
+    char* memory_head  = memory_block;
     libxs_CSR_sparseslice* libxs_output_csr_a = (libxs_CSR_sparseslice*)(memory_head);
     memory_head += handle->mb * handle->kb * sizeof(libxs_CSR_sparseslice);
-    assert(0 != memory_block/*sanity check*/);
+    assert(0 != libxs_output_csr_a/*sanity check*/);
 
     for (kb = 0; kb < k_blocks; kb++) {
       for (mb = 0; mb < m_blocks; mb++) {
