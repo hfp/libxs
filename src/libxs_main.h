c5473efd19a1a3bfbef6a550850f0bd03b7dcfb7
Hans Pabst <hans.pabst@intel.com>
2017-11-07 15:38:42 -0800

    Added C code for max in fwd

--- src/libxs_main.h
+++ src/libxs_main.h
@@ -172,6 +172,8 @@ struct LIBXS_RETARGETABLE libxs_dnn_layer {
   int use_hybrid_wu_parallelism;
   int weight_copies;
   int compute_batch_stats_in_kernel;
+  int compute_max_in_kernel_fwd;
+  int compute_max_in_kernel_bwd;
   int perform_relu_in_kernel;
   int use_lp_kernel;
   int output_lp_padding;
