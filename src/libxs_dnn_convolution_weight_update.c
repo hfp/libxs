e9691f01ebe0dd9b98ea194ed0165ea14b6e2ff2
Hans Pabst <hans.pabst@intel.com>
2017-11-17 17:19:00 -0800

    Added stream loads/stores for output transformation

--- src/libxs_dnn_convolution_weight_update.c
+++ src/libxs_dnn_convolution_weight_update.c
@@ -108,10 +108,10 @@
       odd_addr_lo = &LIBXS_VLA_ACCESS(6, output, img, ofm1, ij, ii+1, 0, 0,  handle->blocksofm_lp, handle->ofhp, handle->ofwp, handle->ofmblock, handle->fm_lp_block); \
       even_addr_hi = &LIBXS_VLA_ACCESS(6, output, img, ofm1, ij, ii, 8, 0,  handle->blocksofm_lp, handle->ofhp, handle->ofwp, handle->ofmblock, handle->fm_lp_block); \
       odd_addr_hi = &LIBXS_VLA_ACCESS(6, output, img, ofm1, ij, ii+1, 8, 0,  handle->blocksofm_lp, handle->ofhp, handle->ofwp, handle->ofmblock, handle->fm_lp_block); \
-      even_pixel_lo = _mm256_loadu_si256((const union __m256i *) even_addr_lo); \
-      odd_pixel_lo = _mm256_loadu_si256((const union __m256i *) odd_addr_lo); \
-      even_pixel_hi = _mm256_loadu_si256((const union __m256i *) even_addr_hi); \
-      odd_pixel_hi = _mm256_loadu_si256((const union __m256i *) odd_addr_hi); \
+      even_pixel_lo = _mm256_stream_load_si256((const union __m256i *) even_addr_lo); \
+      odd_pixel_lo = _mm256_stream_load_si256((const union __m256i *) odd_addr_lo); \
+      even_pixel_hi = _mm256_stream_load_si256((const union __m256i *) even_addr_hi); \
+      odd_pixel_hi = _mm256_stream_load_si256((const union __m256i *) odd_addr_hi); \
       compressed_lo  = _mm256_unpacklo_epi16(even_pixel_lo, odd_pixel_lo); \
       compressed_hi  = _mm256_unpackhi_epi16(even_pixel_lo, odd_pixel_lo); \
       compressed_lo_store = _mm256_insertf128_si256(compressed_lo_store, _mm256_extractf128_si256(compressed_lo,0), 0); \
@@ -120,8 +120,8 @@
       compressed_hi_store = _mm256_insertf128_si256(compressed_hi_store, _mm256_extractf128_si256(compressed_hi,1), 1); \
       dst_lo = &LIBXS_VLA_ACCESS(6,  tr_output, img, 2*ofm1, ij, half_i, 0, 0, BLOCKSOFM, handle->ofhp, OFWP/2, handle->ofmblock, 2); \
       dst_hi = &LIBXS_VLA_ACCESS(6,  tr_output, img, 2*ofm1, ij, half_i, 8, 0, BLOCKSOFM, handle->ofhp, OFWP/2, handle->ofmblock, 2); \
-      _mm256_storeu_si256((union __m256i *) dst_lo, compressed_lo_store); \
-      _mm256_storeu_si256((union __m256i *) dst_hi, compressed_hi_store); \
+      _mm256_stream_si256((union __m256i *) dst_lo, compressed_lo_store); \
+      _mm256_stream_si256((union __m256i *) dst_hi, compressed_hi_store); \
       compressed_lo  = _mm256_unpacklo_epi16(even_pixel_hi, odd_pixel_hi); \
       compressed_hi  = _mm256_unpackhi_epi16(even_pixel_hi, odd_pixel_hi); \
       compressed_lo_store = _mm256_insertf128_si256(compressed_lo_store, _mm256_extractf128_si256(compressed_lo,0), 0); \
@@ -130,14 +130,14 @@
       compressed_hi_store = _mm256_insertf128_si256(compressed_hi_store, _mm256_extractf128_si256(compressed_hi,1), 1); \
       dst_lo = &LIBXS_VLA_ACCESS(6,  tr_output, img, 2*ofm1+1, ij, half_i, 0, 0, BLOCKSOFM, handle->ofhp, OFWP/2, handle->ofmblock, 2); \
       dst_hi = &LIBXS_VLA_ACCESS(6,  tr_output, img, 2*ofm1+1, ij, half_i, 8, 0, BLOCKSOFM, handle->ofhp, OFWP/2, handle->ofmblock, 2); \
-      _mm256_storeu_si256((union __m256i *) dst_lo, compressed_lo_store); \
-      _mm256_storeu_si256((union __m256i *) dst_hi, compressed_hi_store);
+      _mm256_stream_si256((union __m256i *) dst_lo, compressed_lo_store); \
+      _mm256_stream_si256((union __m256i *) dst_hi, compressed_hi_store);
 
 #define TRANSPOSE_W_HALF_PAIR(img, ofm1, ij, ii, half_i) \
       even_addr_lo = &LIBXS_VLA_ACCESS(6, output, img, ofm1, ij, ii, 0, 0,  handle->blocksofm_lp, handle->ofhp, handle->ofwp, handle->ofmblock, handle->fm_lp_block); \
       even_addr_hi = &LIBXS_VLA_ACCESS(6, output, img, ofm1, ij, ii, 8, 0,  handle->blocksofm_lp, handle->ofhp, handle->ofwp, handle->ofmblock, handle->fm_lp_block); \
-      even_pixel_lo = _mm256_loadu_si256((const union __m256i *) even_addr_lo); \
-      even_pixel_hi = _mm256_loadu_si256((const union __m256i *) even_addr_hi); \
+      even_pixel_lo = _mm256_stream_load_si256((const union __m256i *) even_addr_lo); \
+      even_pixel_hi = _mm256_stream_load_si256((const union __m256i *) even_addr_hi); \
       odd_pixel_lo = _mm256_xor_si256(odd_pixel_lo,odd_pixel_lo); \
       odd_pixel_hi = odd_pixel_lo; \
       compressed_lo  = _mm256_unpacklo_epi16(even_pixel_lo, odd_pixel_lo); \
@@ -148,8 +148,8 @@
       compressed_hi_store = _mm256_insertf128_si256(compressed_hi_store, _mm256_extractf128_si256(compressed_hi,1), 1); \
       dst_lo = &LIBXS_VLA_ACCESS(6,  tr_output, img, 2*ofm1, ij, half_i, 0, 0, BLOCKSOFM, handle->ofhp, OFWP/2, handle->ofmblock, 2); \
       dst_hi = &LIBXS_VLA_ACCESS(6,  tr_output, img, 2*ofm1, ij, half_i, 8, 0, BLOCKSOFM, handle->ofhp, OFWP/2, handle->ofmblock, 2); \
-      _mm256_storeu_si256((union __m256i *) dst_lo, compressed_lo_store); \
-      _mm256_storeu_si256((union __m256i *) dst_hi, compressed_hi_store); \
+      _mm256_stream_si256((union __m256i *) dst_lo, compressed_lo_store); \
+      _mm256_stream_si256((union __m256i *) dst_hi, compressed_hi_store); \
       compressed_lo  = _mm256_unpacklo_epi16(even_pixel_hi, odd_pixel_hi); \
       compressed_hi  = _mm256_unpackhi_epi16(even_pixel_hi, odd_pixel_hi); \
       compressed_lo_store = _mm256_insertf128_si256(compressed_lo_store, _mm256_extractf128_si256(compressed_lo,0), 0); \
@@ -158,9 +158,8 @@
       compressed_hi_store = _mm256_insertf128_si256(compressed_hi_store, _mm256_extractf128_si256(compressed_hi,1), 1); \
       dst_lo = &LIBXS_VLA_ACCESS(6,  tr_output, img, 2*ofm1+1, ij, half_i, 0, 0, BLOCKSOFM, handle->ofhp, OFWP/2, handle->ofmblock, 2); \
       dst_hi = &LIBXS_VLA_ACCESS(6,  tr_output, img, 2*ofm1+1, ij, half_i, 8, 0, BLOCKSOFM, handle->ofhp, OFWP/2, handle->ofmblock, 2); \
-      _mm256_storeu_si256((union __m256i *) dst_lo, compressed_lo_store); \
-      _mm256_storeu_si256((union __m256i *) dst_hi, compressed_hi_store);
-
+      _mm256_stream_si256((union __m256i *) dst_lo, compressed_lo_store); \
+      _mm256_stream_si256((union __m256i *) dst_hi, compressed_hi_store);
 
 void lp_transpose_input_and_output(int img, libxs_dnn_layer* handle) {
   typedef short element_input_type;
@@ -921,6 +920,13 @@ void lp_transpose_and_resize_1_chunk_1_remainder_even_pixels(int img, libxs_dnn_
 }
 #endif
 
+#undef TRANSPOSE_W_CHUNK
+#undef TRANSPOSE_W_REMAINDER
+#undef TRANSPOSE_W_FULL_PAIR
+#undef TRANSPOSE_W_HALF_PAIR
+#undef TRANSPOSE_W_CHUNK_RESIZED
+#undef TRANSPOSE_W_REMAINDER_RESIZED
+
 #if 1
 void gather_transpose_ps_16_56_56_16(int M, int N, float *LIBXS_RESTRICT dst, int ldD, const float *LIBXS_RESTRICT src, int ldS) {
   const __m512i vindex = _mm512_set_epi32(240,224,208,192,176,160,144,128,112,96,80,64,48,32,16,0);
@@ -1546,9 +1552,3 @@ LIBXS_API_DEFINITION libxs_dnn_err_t libxs_dnn_convolve_st_upd_nhwc_custom(libxs
 }
 
 
-#undef TRANSPOSE_W_CHUNK
-#undef TRANSPOSE_W_REMAINDER
-#undef TRANSPOSE_W_FULL_PAIR
-#undef TRANSPOSE_W_HALF_PAIR
-#undef TRANSPOSE_W_CHUNK_RESIZED
-#undef TRANSPOSE_W_REMAINDER_RESIZED
