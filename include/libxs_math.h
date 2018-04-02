d39e7215431ec4baf7e4d2a09bc326342a9f64f7
Hans Pabst <hans.pabst@intel.com>
2018-04-02 23:04:57 +0200

    Merge branch 'master' into develop

--- include/libxs_math.h
+++ include/libxs_math.h
@@ -94,6 +94,9 @@ LIBXS_API float libxs_sexp2_u8(unsigned char x);
 */
 LIBXS_API float libxs_sexp2_i8(signed char x);
 
+/** Similar to libxs_sexp2_i8, but takes an integer as signed 8-bit value (check). */
+LIBXS_API float libxs_sexp2_i8i(int x);
+
 /** Function to seed libxs_rand_u32 (similar to srand). */
 LIBXS_API void libxs_srand(unsigned int seed);
 
