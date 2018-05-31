d231c3a2fd8b432c00826082d2da471921bc4c5a
Hans Pabst <hans.pabst@intel.com>
2018-05-31 13:42:57 +0200

    Fixed warnings about external function definitions with no prior declaration (DNN). Prefixed public/API function to carry libxs_-prefix.

--- src/libxs_dnn.c
+++ src/libxs_dnn.c
@@ -3014,7 +3014,7 @@ LIBXS_API void libxs_rnaz_convert_fp32_bfp16(const float* in, libxs_bfloat16* ou
 }
 
 
-LIBXS_API void rne_convert_fp32_bfp16(const float* in, libxs_bfloat16* out, const unsigned int len) {
+LIBXS_API void libxs_rne_convert_fp32_bfp16(const float* in, libxs_bfloat16* out, const unsigned int len) {
   unsigned int i = 0;
 
   /* truncate buffer to bfp16 */
