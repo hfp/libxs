bcc9f5f38a90e2672cfd1284167831f56680dcad
Hans Pabst <hans.pabst@intel.com>
2018-07-12 20:51:03 -0700

    Fixed condition for img par

--- src/libxs_dnn_dryruns.c
+++ src/libxs_dnn_dryruns.c
@@ -126,7 +126,7 @@ LIBXS_API_INTERN libxs_dnn_err_t libxs_dnn_perform_fwd_dryrun_direct_custom_cust
 # include "template/libxs_dnn_convolve_dryrun_fwd_custom_custom_bf16.tpl.c"
     } else {
       /* TODO: Second condition guarantees we run the img_par code when we have MB=1 -- and hopefully HUGE images */
-      if ((handle->desc.N*handle->blocksofm >= handle->desc.threads) && !((handle->desc.N == 1) && (handle->fwd_ofh_rb == 1 )) ) {
+      if ((handle->desc.N*handle->blocksofm >= handle->desc.threads) && !((handle->desc.N == 1) && (handle->fwd_ofh_rb == 1) && (handle->n_variants == 1)) ) {
 # include "template/libxs_dnn_convolve_dryrun_fwd_custom_custom.tpl.c"
       } else {
 # include "template/libxs_dnn_convolve_dryrun_fwd_custom_custom_img_par.tpl.c"
