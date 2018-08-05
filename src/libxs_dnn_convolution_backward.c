c6d4ef8b8c909682e69bc245042ad28c930bb884
Hans Pabst <hans.pabst@intel.com>
2018-08-05 20:20:27 +0200

    Merge branch 'master' of https://github.com/hfp/libxs

--- src/libxs_dnn_convolution_backward.c
+++ src/libxs_dnn_convolution_backward.c
@@ -42,6 +42,7 @@
 
 
 LIBXS_API_INTERN libxs_dnn_err_t libxs_dnn_convolve_st_bwd_custom_custom_f32_f32(libxs_dnn_layer* handle, int start_thread, int tid);
+LIBXS_API_INTERN libxs_dnn_err_t libxs_dnn_convolve_st_bwd_custom_custom_bf16_bf16(libxs_dnn_layer* handle, int start_thread, int tid);
 LIBXS_API_INTERN libxs_dnn_err_t libxs_dnn_convolve_st_bwd_custom_custom_i16_i32(libxs_dnn_layer* handle, int start_thread, int tid);
 LIBXS_API_INTERN libxs_dnn_err_t libxs_dnn_convolve_st_bwd_custom_custom_i16_f32(libxs_dnn_layer* handle, int start_thread, int tid);
 LIBXS_API_INTERN libxs_dnn_err_t libxs_dnn_convolve_st_bwd_custom_custom_i8_i32(libxs_dnn_layer* handle, int start_thread, int tid);
@@ -56,6 +57,24 @@ libxs_dnn_err_t libxs_dnn_convolve_st_bwd_custom_custom_f32_f32(libxs_dnn_layer*
   typedef float element_output_type;
   typedef float element_filter_type;
   typedef libxs_sconvfunction libxs_convfunction;
+# include "template/libxs_dnn_convolve_st_bwd_via_fwd_custom_custom_stream.tpl.c"
+#else /* should not happen */
+  LIBXS_UNUSED(handle); LIBXS_UNUSED(start_thread); LIBXS_UNUSED(tid);
+  status = LIBXS_DNN_ERR_UNSUPPORTED_ARCH;
+#endif
+  return status;
+}
+
+
+LIBXS_API_INTERN LIBXS_INTRINSICS(LIBXS_X86_AVX512)
+libxs_dnn_err_t libxs_dnn_convolve_st_bwd_custom_custom_bf16_bf16(libxs_dnn_layer* handle, int start_thread, int tid)
+{
+  libxs_dnn_err_t status = LIBXS_DNN_SUCCESS;
+#if defined(LIBXS_INTRINSICS_AVX512) /*__AVX512F__*/
+  typedef libxs_bfloat16 element_input_type;
+  typedef libxs_bfloat16 element_output_type;
+  typedef libxs_bfloat16 element_filter_type;
+  typedef libxs_bf16convfunction libxs_convfunction;
 # include "template/libxs_dnn_convolve_st_bwd_via_fwd_custom_custom_stream.tpl.c"
 #else /* should not happen */
   LIBXS_UNUSED(handle); LIBXS_UNUSED(start_thread); LIBXS_UNUSED(tid);
@@ -169,6 +188,8 @@ LIBXS_API_INTERN libxs_dnn_err_t libxs_dnn_convolve_st_bwd_custom_custom(libxs_d
       status = libxs_dnn_convolve_st_bwd_custom_custom_f32_f32( handle, start_thread, tid );
     } else if (handle->datatype_in == LIBXS_DNN_DATATYPE_I16 && handle->datatype_out == LIBXS_DNN_DATATYPE_I32 ) {
       status = libxs_dnn_convolve_st_bwd_custom_custom_i16_i32( handle, start_thread, tid );
+    } else if (handle->datatype_in == LIBXS_DNN_DATATYPE_BF16 && handle->datatype_out == LIBXS_DNN_DATATYPE_BF16 ) {
+      status = libxs_dnn_convolve_st_bwd_custom_custom_bf16_bf16( handle, start_thread, tid );
     } else if (handle->datatype_in ==  LIBXS_DNN_DATATYPE_I16 && handle->datatype_out == LIBXS_DNN_DATATYPE_F32 ) {
       status = libxs_dnn_convolve_st_bwd_custom_custom_i16_f32( handle, start_thread, tid );
     } else if (handle->datatype_in == LIBXS_DNN_DATATYPE_I8 && handle->datatype_out == LIBXS_DNN_DATATYPE_I32 && (handle->desc.options & LIBXS_DNN_CONV_OPTION_ACTIVATION_UNSIGNED) > 0 ) {
