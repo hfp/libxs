2b78ee370d020f94802b8f56e345694b2e701beb
Hans Pabst <hans.pabst@intel.com>
2018-08-11 17:17:02 +0200

    Renamed libxs_split_work into libxs_product_limit (more adequate name for foundational function/algorithm); renamed function-calls accordingly. Improved algorithm when LIBXS_MATH_MAXPRODUCT is exceeded (libxs_product_limit). Introduced is_lower limit to at least reached or exceeded with the minimal possible product (libxs_product_limit); included tests (tests/math.c). Adjusted parameterization of tiles, and privatized previously global variables (XGEMM). Refactored work-plan into libxs_gemm_plan_internal. Avoid any remainder tiles and plan tile sizes accordingly.

--- src/libxs_math.c
+++ src/libxs_math.c
@@ -43,8 +43,8 @@
 # pragma offload_attribute(pop)
 #endif
 
-#if !defined(LIBXS_MAX_SPLITLIMIT)
-# define LIBXS_MAX_SPLITLIMIT 1024
+#if !defined(LIBXS_MATH_MAXPRODUCT)
+# define LIBXS_MATH_MAXPRODUCT 1024
 #endif
 
 
@@ -207,55 +207,85 @@ LIBXS_API int libxs_primes_u32(unsigned int num, unsigned int num_factors_n32[])
 }
 
 
-LIBXS_API unsigned int libxs_split_work(unsigned int work, unsigned int split_limit)
+LIBXS_API unsigned int libxs_product_limit(unsigned int product, unsigned int limit, int is_lower)
 {
   int result;
-  if (split_limit < work) {
+  if (limit < product) {
     result = 1;
-    if (1 < split_limit) {
-      unsigned int fact[32], wmax = split_limit;
-      int i;
+    if (1 < limit) {
+      unsigned int fact[32], maxp = limit;
+      int i, n;
       /* attempt to lower the memory requirement for DP; can miss best solution */
-      if (LIBXS_MAX_SPLITLIMIT < split_limit) {
-        result = (unsigned int)libxs_gcd(work, split_limit);
-        wmax /= result;
+      if (LIBXS_MATH_MAXPRODUCT < limit) {
+        const unsigned int minfct = (limit + limit - 1) / LIBXS_MATH_MAXPRODUCT;
+        const unsigned int maxfct = (unsigned int)libxs_gcd(product, limit);
+        result = maxfct;
+        if (minfct < maxfct) {
+          n = libxs_primes_u32(result, fact);
+          for (i = 0; i < n; ++i) {
+            if (minfct < fact[i]) {
+              result = fact[i];
+              i = n; /* break */
+            }
+          }
+        }
+        maxp /= result;
       }
-      if (LIBXS_MAX_SPLITLIMIT >= wmax) {
-        unsigned int k[2][LIBXS_MAX_SPLITLIMIT], w;
-        const int n = libxs_primes_u32(work / result, fact);
-        unsigned int *k0 = k[0], *k1 = k[1], *kt;
+      if (LIBXS_MATH_MAXPRODUCT >= maxp) {
+        unsigned int k[2][LIBXS_MATH_MAXPRODUCT], *k0 = k[0], *k1 = k[1], *kt, p;
+        n = libxs_primes_u32(product / result, fact);
         /* initialize table with trivial factor */
-        for (w = 0; w <= wmax; ++w) k[0][w] = 1;
+        for (p = 0; p <= maxp; ++p) k[0][p] = 1;
         k[0][0] = k[1][0] = 1;
         for (i = 1; i <= n; ++i) {
-          for (w = 1; w <= wmax; ++w) {
-            const unsigned int f = fact[i-1], h = k0[w];
-            if (w < f) {
-              k1[w] = h;
+          for (p = 1; p <= maxp; ++p) {
+            const unsigned int f = fact[i - 1], h = k0[p];
+            if (p < f) {
+              k1[p] = h;
             }
             else {
-              const unsigned int g = f * k0[w/f];
-              k1[w] = LIBXS_MAX(g, h);
+              const unsigned int g = f * k0[p / f];
+              k1[p] = LIBXS_MAX(g, h);
             }
           }
           kt = k0; k0 = k1; k1 = kt;
         }
-        result *= k0[wmax];
+        result *= k0[maxp];
       }
       else { /* trivial approximation */
-        const int n = libxs_primes_u32(work, fact);
+        n = libxs_primes_u32(product, fact);
         for (i = 0; i < n; ++i) {
           const unsigned int f = result * fact[i];
-          if (f <= split_limit) {
+          if (f <= limit) {
             result = f;
           }
           else i = n; /* break */
         }
       }
+      if (0 != is_lower) {
+        unsigned int rfact[32];
+        const int m = libxs_primes_u32(result, rfact);
+        if (0 != m) {
+          int j = 0;
+          n = libxs_primes_u32(product, fact);
+          LIBXS_ASSERT(m <= n);
+          for (i = 0; i < n; ++i) {
+            const unsigned int fi = fact[i];
+            while (fi == rfact[j] && j < m) {
+              ++i;
+              ++j;
+            }
+            if (j == m || fi != rfact[j]) {
+              result *= fi;
+              i = n; /* break */
+            }
+          }
+        }
+      }
     }
   }
   else { /* fast-path */
-    result = work;
+    result = product;
   }
   return result;
 }
@@ -339,7 +369,7 @@ LIBXS_API unsigned int libxs_icbrt_u32(unsigned int x)
   return y;
 }
 
-/* Implementation based on Claude Baumann's work (http://www.convict.lu/Jeunes/ultimate_stuff/exp_ln_2.htm). */
+/* Implementation based on Claude Baumann's product (http://www.convict.lu/Jeunes/ultimate_stuff/exp_ln_2.htm). */
 LIBXS_API float libxs_sexp2_fast(float x, int maxiter)
 {
   static const float lut[] = { /* tabulated powf(2.f, powf(2.f, -index)) */
