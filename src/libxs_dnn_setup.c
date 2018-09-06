5bb26f077a44e27b944a4ee87084fbd536bd7367
Hans Pabst <hans.pabst@intel.com>
2018-09-06 15:03:14 -0700

    Fixed performance for first resnet layer upd bf16

--- src/libxs_dnn_setup.c
+++ src/libxs_dnn_setup.c
@@ -1328,8 +1328,8 @@ LIBXS_API_INTERN libxs_dnn_err_t libxs_dnn_setup_upd( libxs_dnn_layer* handle, i
 
         /* FIXME: Specific code generation for first layer of resnet50 */
         if ( handle->datatype_in == LIBXS_DNN_DATATYPE_BF16 && handle->use_fastpath == 0) {
-          descriptor.ofw_rb = handle->ofw;
-          descriptor.ofh_rb = handle->ofh;
+          descriptor.ofw_rb = 28;
+          descriptor.ofh_rb = 4;
           handle->upd_ofh_rb = descriptor.ofh_rb;
           handle->upd_ofw_rb = descriptor.ofw_rb;
           descriptor.transpose_ofw_ifm = 0;
