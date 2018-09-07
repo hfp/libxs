785fc6cf999f951fa9c2fc2a60cf052b9f931e02
Hans Pabst <hans.pabst@intel.com>
2018-09-07 17:18:32 +0200

    Improved dispatch micro-benchmark [skip ci].

--- samples/registry/registry.c
+++ samples/registry/registry.c
@@ -57,15 +57,17 @@ int main(int argc, char* argv[])
 #else
   const int max_nthreads = 1;
 #endif
+  const int default_minsize = 4, default_maxsize = 16;
   const int size = LIBXS_MAX(1 < argc ? atoi(argv[1]) : 10000/*default*/, 1);
   const int nthreads = LIBXS_CLMP(2 < argc ? atoi(argv[2]) : 1/*default*/, 1, max_nthreads);
-  const libxs_blasint maxksize = LIBXS_CLMP(3 < argc ? atoi(argv[3]) : 16/*default*/, 1, LIBXS_MAX_M);
-  const libxs_blasint minksize = LIBXS_CLMP(4 < argc ? atoi(argv[4]) : 4/*default*/, 1, maxksize);
-  libxs_timer_tickint tdsp0 = 0, tdsp1 = 0, tcgen = 0, tcall;
+  const libxs_blasint maxksize = LIBXS_CLMP(3 < argc ? atoi(argv[3]) : default_maxsize, 1, LIBXS_MAX_M);
+  const libxs_blasint minksize = LIBXS_CLMP(4 < argc ? atoi(argv[4]) : default_minsize, 1, maxksize);
   const libxs_blasint krange = maxksize - minksize;
+  double tcall, tdsp0, tdsp1, tcgen;
+  libxs_timer_tickint start;
   int result = EXIT_SUCCESS;
 
-  fprintf(stdout, "Dispatching %i calls %s internal synchronization using %i thread%s...\n", size,
+  printf("Dispatching %i calls %s internal synchronization using %i thread%s...\n", size,
 #if (0 != LIBXS_SYNC)
     "with",
 #else
@@ -89,9 +91,10 @@ int main(int argc, char* argv[])
     libxs_blasint *const rm = (libxs_blasint*)malloc(size * sizeof(libxs_blasint));
     libxs_blasint *const rn = (libxs_blasint*)malloc(size * sizeof(libxs_blasint));
     libxs_blasint *const rk = (libxs_blasint*)malloc(size * sizeof(libxs_blasint));
-    const libxs_blasint m0 = (1 < krange ? ((23 % krange) + minksize) : minksize);
-    const libxs_blasint n0 = (1 < krange ? ((23 % krange) + minksize) : minksize);
-    const libxs_blasint k0 = (1 < krange ? ((23 % krange) + minksize) : minksize);
+    const libxs_blasint m0 = (1 < krange ? ((default_maxsize % krange) + minksize) : minksize);
+    const libxs_blasint n0 = (1 < krange ? ((default_maxsize % krange) + minksize) : minksize);
+    const libxs_blasint k0 = (1 < krange ? ((default_maxsize % krange) + minksize) : minksize);
+    const size_t shuffle = libxs_shuffle(size);
     const double alpha = 1, beta = 1;
     int i;
 
@@ -99,7 +102,8 @@ int main(int argc, char* argv[])
     void* *const jitter = malloc(size * sizeof(void*));
     if (NULL == jitter) exit(EXIT_FAILURE);
 #else
-    libxs_registry_info reginfo;
+    const int prefetch = LIBXS_GEMM_PREFETCH_NONE;
+    const int flags = LIBXS_GEMM_FLAG_NONE;
 #endif
     if (NULL == rm && NULL == rn && NULL == rk) exit(EXIT_FAILURE);
 
@@ -115,109 +119,132 @@ int main(int argc, char* argv[])
 
     /* first invocation may initialize some internals */
     libxs_init(); /* subsequent calls are not doing any work */
-    tcall = libxs_timer_tick();
+    start = libxs_timer_tick();
     for (i = 0; i < size; ++i) {
       /* measure call overhead of an "empty" function (not inlined) */
       libxs_init();
     }
-    tcall = libxs_timer_diff(tcall, libxs_timer_tick());
+    tcall = libxs_timer_duration(start, libxs_timer_tick());
 
     { /* trigger code generation to subsequently measure only dispatch time */
 #if defined(mkl_jit_create_dgemm)
-      LIBXS_EXPECT(MKL_JIT_SUCCESS, mkl_cblas_jit_create_dgemm(jitter, MKL_COL_MAJOR, MKL_NOTRANS/*transa*/, MKL_NOTRANS/*transb*/,
+      LIBXS_EXPECT(MKL_JIT_SUCCESS, mkl_cblas_jit_create_dgemm(jitter,
+        MKL_COL_MAJOR, MKL_NOTRANS/*transa*/, MKL_NOTRANS/*transb*/,
         m0, n0, k0, alpha, m0, k0, beta, m0));
       LIBXS_EXPECT_NOT(NULL, mkl_jit_get_dgemm_ptr(jitter[0])); /* to measure "cached" lookup time (below) */
 #else
-      LIBXS_EXPECT_NOT(NULL, libxs_dmmdispatch(m0, n0, k0, &m0, &k0, &m0, &alpha, &beta, NULL/*flags*/, NULL/*prefetch*/));
+      LIBXS_EXPECT_NOT(NULL, libxs_dmmdispatch(m0, n0, k0, &m0, &k0, &m0, &alpha, &beta, &flags, &prefetch));
 #endif
     }
 
-    /* measure dispatching previously generated and eventually cached kernel */
+    /* measure dispatching previously generated (likely cached) */
 #if defined(_OPENMP)
-#   pragma omp parallel for num_threads(nthreads) private(i)
-#endif
-    for (i = 0; i < size; ++i) {
-      const libxs_timer_tickint t0 = libxs_timer_tick();
-#if defined(mkl_jit_create_dgemm)
-      mkl_jit_get_dgemm_ptr(jitter[0]);
+#   pragma omp parallel num_threads(nthreads) private(i)
+    {
+#     pragma omp single
+      start = libxs_timer_tick();
+#     pragma omp for
 #else
-      libxs_dmmdispatch(m0, n0, k0, &m0, &k0, &m0, &alpha, &beta, NULL/*flags*/, NULL/*prefetch*/);
+    {
+      start = libxs_timer_tick();
 #endif
-#if defined(_OPENMP)
-#     pragma omp atomic
+      for (i = 0; i < size; ++i) {
+#if defined(mkl_jit_create_dgemm)
+        mkl_jit_get_dgemm_ptr(jitter[0]); /* no "dispatch" just unwrapping the jitter */
+#else
+        libxs_dmmdispatch(m0, n0, k0, &m0, &k0, &m0, &alpha, &beta, &flags, &prefetch);
 #endif
-      tdsp1 += libxs_timer_diff(t0, libxs_timer_tick());
+      }
     }
+    tdsp1 = libxs_timer_duration(start, libxs_timer_tick());
 #if defined(mkl_jit_create_dgemm)
     mkl_jit_destroy(jitter[0]); jitter[0] = NULL;
 #endif
 
     /* measure generating JIT-kernels (randomized parameterization) */
 #if defined(_OPENMP)
-#   pragma omp parallel for num_threads(nthreads) private(i)
-#endif
-    for (i = 0; i < size; ++i) {
-      const libxs_timer_tickint t0 = libxs_timer_tick();
-#if defined(mkl_jit_create_dgemm)
-      LIBXS_EXPECT(MKL_JIT_SUCCESS, mkl_cblas_jit_create_dgemm(jitter + i, MKL_COL_MAJOR, MKL_NOTRANS/*transa*/, MKL_NOTRANS/*transb*/,
-        rm[i], rn[i], rk[i], alpha, rm[i], rk[i], beta, rm[i])); /* generate */
-      LIBXS_EXPECT_NOT(NULL, mkl_jit_get_dgemm_ptr(jitter[i])); /* ...and dispatch (no release) */
+#   pragma omp parallel num_threads(nthreads) private(i)
+    {
+#     pragma omp single
+      start = libxs_timer_tick();
+#     pragma omp for
 #else
-      LIBXS_EXPECT_NOT(NULL, libxs_dmmdispatch(rm[i], rn[i], rk[i],
-        rm + i, rk + i, rm + i, &alpha, &beta, NULL/*flags*/, NULL/*prefetch*/));
+    {
+      start = libxs_timer_tick();
 #endif
-#if defined(_OPENMP)
-#     pragma omp atomic
-#endif
-      tcgen += libxs_timer_diff(t0, libxs_timer_tick());
-    }
-
-#if !defined(mkl_jit_create_dgemm)
-    /* correct for duplicated code generation requests */
-    if (EXIT_SUCCESS == libxs_get_registry_info(&reginfo)) {
-      const int ncgens = (int)(reginfo.size - 1/*initial code gen.*/);
-      tcgen -= tdsp1 * (size - ncgens) / size;
-    }
-#endif
-
-    /* measure dispatching previously generated kernel */
-#if defined(_OPENMP)
-#   pragma omp parallel for num_threads(nthreads) private(i)
-#endif
-    for (i = 0; i < size; ++i) {
-      const libxs_timer_tickint t0 = libxs_timer_tick();
+      for (i = 1; i < size; ++i) {
 #if defined(mkl_jit_create_dgemm)
-      LIBXS_EXPECT_NOT(NULL, mkl_jit_get_dgemm_ptr(jitter[i])); /* dispatch (likely uncached) */
+        LIBXS_EXPECT(MKL_JIT_SUCCESS, mkl_cblas_jit_create_dgemm(jitter + i,
+          MKL_COL_MAJOR, MKL_NOTRANS/*transa*/, MKL_NOTRANS/*transb*/,
+          rm[i], rn[i], rk[i], alpha, rm[i], rk[i], beta, rm[i]));
+        LIBXS_EXPECT_NOT(NULL, mkl_jit_get_dgemm_ptr(jitter[i]));
 #else
-      LIBXS_EXPECT_NOT(NULL, libxs_dmmdispatch(rm[i], rn[i], rk[i],
-        rm + i, rk + i, rm + i, &alpha, &beta, NULL/*flags*/, NULL/*prefetch*/));
+        LIBXS_EXPECT_NOT(NULL, libxs_dmmdispatch(rm[i], rn[i], rk[i],
+          rm + i, rk + i, rm + i, &alpha, &beta, &flags, &prefetch));
 #endif
-#if defined(_OPENMP)
-#     pragma omp atomic
-#endif
-      tdsp0 += libxs_timer_diff(t0, libxs_timer_tick());
+      }
     }
+    tcgen = libxs_timer_duration(start, libxs_timer_tick());
 
-    free(rm); free(rn); free(rk); /* release random numbers */
+    /* measure dispatching previously generated kernel (likely non-cached) */
+#if defined(_OPENMP)
+#   pragma omp parallel num_threads(nthreads) private(i)
+    {
+#     pragma omp single
+      start = libxs_timer_tick();
+#     pragma omp for
+#else
+    {
+      start = libxs_timer_tick();
+#endif
+      for (i = 0; i < size; ++i) {
+        const int j = (int)((shuffle * i) % size);
 #if defined(mkl_jit_create_dgemm)
-    for (i = 0; i < size; ++i) mkl_jit_destroy(jitter[i]); /* release dispatched code */
+        LIBXS_EXPECT_NOT(NULL, mkl_jit_get_dgemm_ptr(jitter[j]));
+#else
+        LIBXS_EXPECT_NOT(NULL, libxs_dmmdispatch(rm[j], rn[j], rk[j],
+          rm + j, rk + j, rm + j, &alpha, &beta, &flags, &prefetch));
+#endif
+      }
+    }
+    tdsp0 = libxs_timer_duration(start, libxs_timer_tick());
+
+    /* release random numbers */
+    free(rm); free(rn); free(rk);
+#if defined(mkl_jit_create_dgemm) /* release dispatched code */
+    for (i = 0; i < size; ++i) mkl_jit_destroy(jitter[i]);
     free(jitter); /* release array used to store dispatched code */
 #endif
   }
 
-  if (0 < size) {
-    const double dcall = libxs_timer_duration(0, tcall) / size;
-    const double ddsp0 = libxs_timer_duration(0, tdsp0) / size;
-    const double ddsp1 = libxs_timer_duration(0, tdsp1) / size;
-    const double dcgen = libxs_timer_duration(0, tcgen) / size;
+  if (1 < size) {
+    tcall /= size; tdsp0 /= size; tdsp1 /= size;
+#if !defined(mkl_jit_create_dgemm)
+    { /* correct for duplicated code generation requests */
+      libxs_registry_info reginfo;
+      if (EXIT_SUCCESS == libxs_get_registry_info(&reginfo)) {
+        const int ngen = (int)reginfo.size;
+        LIBXS_ASSERT(1 < ngen);
+        tcgen -= tdsp0 * (size - ngen);
+        tcgen /= ngen - 1;
+      }
+    }
+#else
+    tcgen /= size - 1;
+#endif
     if (0 < tcall && 0 < tdsp0 && 0 < tdsp1 && 0 < tcgen) {
-      fprintf(stdout, "\tfunction-call (empty): %.0f ns (%.0f MHz)\n", 1E9 * dcall, 1E-6 / dcall);
-      fprintf(stdout, "\tdispatch (ro/cached): %.0f ns (%.0f MHz)\n", 1E9 * ddsp1, 1E-6 / ddsp1);
-      fprintf(stdout, "\tdispatch (ro): %.0f ns (%.0f MHz)\n", 1E9 * ddsp0, 1E-6 / ddsp0);
-      fprintf(stdout, "\tcode-gen (rw): %.0f us (%.0f kHz)\n", 1E6 * dcgen, 1E-3 / dcgen);
+      printf("\tfunction-call (empty): %.0f ns (%.0f MHz)\n", 1E9 * tcall, 1E-6 / tcall);
+      printf("\tdispatch (ro/cached): %.0f ns (%.0f MHz)\n", 1E9 * tdsp1, 1E-6 / tdsp1);
+      printf("\tdispatch (ro): %.0f ns (%.0f MHz)\n", 1E9 * tdsp0, 1E-6 / tdsp0);
+      if (1E-6 <= tcgen) {
+        printf("\tcode-gen (rw): %.0f us (%.0f kHz)\n", 1E6 * tcgen, 1E-3 / tcgen);
+      }
+      else {
+        printf("\tcode-gen (rw): %.0f ns (%.0f MHz)\n", 1E9 * tcgen, 1E-6 / tcgen);
+      }
     }
   }
-  fprintf(stdout, "Finished\n");
+  printf("Finished\n");
 
   return result;
 }
