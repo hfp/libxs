d3ed78c970fc74c7cba45e73ae6aab89aa6cfd0b
Hans Pabst <hans.pabst@intel.com>
2018-09-09 11:37:41 +0200

    Merge branch 'master' of https://github.com/hfp/libxs into develop

--- src/libxs_dnn_pooling.c
+++ src/libxs_dnn_pooling.c
@@ -35,10 +35,10 @@
 # pragma offload_attribute(push,target(LIBXS_OFFLOAD_TARGET))
 #endif
 #include <string.h>
+#include <math.h>
 #if defined(LIBXS_OFFLOAD_TARGET)
 # pragma offload_attribute(pop)
 #endif
-#include <math.h>
 
 LIBXS_API libxs_dnn_pooling* libxs_dnn_create_pooling(libxs_dnn_pooling_desc pooling_desc, libxs_dnn_err_t* status) {
   libxs_dnn_pooling* handle = 0;
