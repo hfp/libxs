6233421c48f2adc2cae1d7d372d10ab674372c8b
Hans Pabst <hans.pabst@intel.com>
2018-09-10 17:44:34 +0200

    Code cleanup.

--- src/libxs_math.c
+++ src/libxs_math.c
@@ -26,7 +26,6 @@
 ** NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS        **
 ** SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.              **
 ******************************************************************************/
-
 #include <libxs_math.h>
 #include <libxs_mhd.h>
 #include "libxs_hash.h"
