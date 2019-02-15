7e458cb926c15f869a5b9aa60708dccc11d88aa3
Hans Pabst <hans.pabst@intel.com>
2019-02-15 12:42:26 +0100

    Fixed some search'n'replace issues/typos.

--- include/libxs_utils.h
+++ include/libxs_utils.h
@@ -567,8 +567,8 @@ LIBXS_API_INLINE LIBXS_INTRINSICS(LIBXS_X86_AVX512) __m512i LIBXS_INTRINSICS_MM5
 # include <math.h>
 LIBXS_API_INLINE LIBXS_INTRINSICS(LIBXS_X86_AVX512) __m512 LIBXS_INTRINSICS_MM512_TANH_PS(__m512 a) {
   float a16[16]; int i;
-  _mm512_store_ps(a16, x);
-  for (i = 0; i < 16; ++i) a16[i] = LIBXS_TANHF(_x[i]);
+  _mm512_store_ps(a16, a);
+  for (i = 0; i < 16; ++i) a16[i] = LIBXS_TANHF(a16[i]);
   return _mm512_loadu_ps(a16);
 }
 #endif /* SVML */
