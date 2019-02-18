81bd19018c0f790da94fbf864337ab64a761df2c
Hans Pabst <hans.pabst@intel.com>
2019-02-18 11:06:37 +0100

    Incorporated several statistical measures into libxs_matdiff_info (libxs_matdiff). The function libxs_matdiff uses compensated numerics and also considers NaNs.

--- include/libxs_math.h
+++ include/libxs_math.h
@@ -40,10 +40,12 @@ LIBXS_EXTERN_C typedef struct LIBXS_RETARGETABLE libxs_matdiff_info {
   /** One-norm */         double norm1_abs, norm1_rel;
   /** Infinity-norm */    double normi_abs, normi_rel;
   /** Froebenius-norm */  double normf_rel;
-  /** L1-norm and L2-norm of differences. */
-  double l2_abs, l2_rel, l1_ref, l1_tst;
-  /** Maximum absolute and relative difference. */
-  double linf_abs, linf_rel;
+  /** Maximum difference, and L2-norm (both absolute and relative). */
+  double linf_abs, linf_rel, l2_abs, l2_rel;
+  /** Statistics: sum/l1, min., max., arith. avg., and variance. */
+  double l1_ref, min_ref, max_ref, avg_ref, var_ref;
+  /** Statistics: sum/l1, min., max., arith. avg., and variance. */
+  double l1_tst, min_tst, max_tst, avg_tst, var_tst;
   /** Location (m, n) of largest difference (linf_abs). */
   libxs_blasint m, n;
 } libxs_matdiff_info;
