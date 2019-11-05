e67147e67f4e20283fc14461f1969d8ebd4b88f6
Hans Pabst <hans.pabst@intel.com>
2019-11-05 14:07:10 -0800

    reworked random number generator to seperate int32 from float portion [skip ci]

--- include/libxs_utils.h
+++ include/libxs_utils.h
@@ -940,21 +940,21 @@ LIBXS_API_INLINE LIBXS_INTRINSICS(LIBXS_X86_AVX512) __m512 LIBXS_INTRINSICS_MM51
   return _mm512_loadu_ps(a16);
 }
 #endif /* SVML */
-/** 2048-bit state for RNG */
+/** 2048-bit state for xoshiro128+ RNG */
 #define LIBXS_INTRINSICS_MM512_RNG_STATE(INDEX) (*(__m512i*)LIBXS_CONCATENATE(libxs_intrinsics_mm512_rng_state, INDEX))
 LIBXS_APIVAR_ARRAY(unsigned int libxs_intrinsics_mm512_rng_state0, 16);
 LIBXS_APIVAR_ARRAY(unsigned int libxs_intrinsics_mm512_rng_state1, 16);
 LIBXS_APIVAR_ARRAY(unsigned int libxs_intrinsics_mm512_rng_state2, 16);
 LIBXS_APIVAR_ARRAY(unsigned int libxs_intrinsics_mm512_rng_state3, 16);
 
-/** Generate random number in the interval [0, 1); not thread-safe. */
+/** Generate random number in the interval [0, 1); not thread-safe.
+ *  this is based on xoshiro128+ 1.0, e.g. http://prng.di.unimi.it/xoshiro128plus.c */
 # if defined(__GNUC__) && !defined(__clang__) && !defined(LIBXS_INTEL_COMPILER) && !defined(_CRAYC) && 0
 LIBXS_PRAGMA_OPTIMIZE_OFF /* avoid ICE in case of symbols (-g) */
 # endif
-LIBXS_API_INLINE LIBXS_INTRINSICS(LIBXS_X86_AVX512) __m512 LIBXS_INTRINSICS_MM512_RNG_PS(void) {
-  const __m512i rng_mantissa = _mm512_srli_epi32(_mm512_add_epi32(LIBXS_INTRINSICS_MM512_RNG_STATE(0), LIBXS_INTRINSICS_MM512_RNG_STATE(3)), 9);
+LIBXS_API_INLINE LIBXS_INTRINSICS(LIBXS_X86_AVX512) __m512i LIBXS_INTRINSICS_MM512_RNG_XOSHIRO128P_EPI32(void) {
+  const __m512i result = _mm512_add_epi32(LIBXS_INTRINSICS_MM512_RNG_STATE(0), LIBXS_INTRINSICS_MM512_RNG_STATE(3));
   const __m512i s = _mm512_slli_epi32(LIBXS_INTRINSICS_MM512_RNG_STATE(1), 9);
-  const __m512 one = _mm512_set1_ps(1.0f);
   __m512i t;
   LIBXS_INTRINSICS_MM512_RNG_STATE(2) = _mm512_xor_epi32(LIBXS_INTRINSICS_MM512_RNG_STATE(2), LIBXS_INTRINSICS_MM512_RNG_STATE(0));
   LIBXS_INTRINSICS_MM512_RNG_STATE(3) = _mm512_xor_epi32(LIBXS_INTRINSICS_MM512_RNG_STATE(3), LIBXS_INTRINSICS_MM512_RNG_STATE(1));
@@ -963,6 +963,12 @@ LIBXS_API_INLINE LIBXS_INTRINSICS(LIBXS_X86_AVX512) __m512 LIBXS_INTRINSICS_MM51
   LIBXS_INTRINSICS_MM512_RNG_STATE(2) = _mm512_xor_epi32(LIBXS_INTRINSICS_MM512_RNG_STATE(2), s);
   t = _mm512_slli_epi32(LIBXS_INTRINSICS_MM512_RNG_STATE(3), 11);
   LIBXS_INTRINSICS_MM512_RNG_STATE(3) = _mm512_or_epi32(t, _mm512_srli_epi32(LIBXS_INTRINSICS_MM512_RNG_STATE(3), 32 - 11));
+  return result;
+}
+
+LIBXS_API_INLINE LIBXS_INTRINSICS(LIBXS_X86_AVX512) __m512 LIBXS_INTRINSICS_MM512_RNG_PS(void) {
+  const __m512i rng_mantissa = _mm512_srli_epi32( LIBXS_INTRINSICS_MM512_RNG_XOSHIRO128P_EPI32(), 9 );
+  const __m512 one = _mm512_set1_ps(1.0f);
   return _mm512_sub_ps(_mm512_castsi512_ps(_mm512_or_epi32(_mm512_set1_epi32(0x3f800000), rng_mantissa)), one);
 }
 # if defined(__GNUC__) && !defined(__clang__) && !defined(LIBXS_INTEL_COMPILER) && !defined(_CRAYC) && 0
