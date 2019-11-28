ec3aefb779193337d031965c1c6355481482e9d5
Hans Pabst <hans.pabst@intel.com>
2019-11-28 12:23:33 +0100

    Issue #358: Added libxs_source.h (it will change if files are renamed added under src directory); saves Bash dependency and calling scripts/libxs_source.sh on platforms (namely Windows) without support for LIBXS's build system (GNU Make). Integrated prebuild step into VS project simply copying header-templates into include.directory (to rely on LIBXS_DEFAULT_CONFIG).

---
+++ include/libxs_source.h
@@ -0,0 +1,133 @@
+/******************************************************************************
+** Copyright (c) 2016-2019, Intel Corporation                                **
+** All rights reserved.                                                      **
+**                                                                           **
+** Redistribution and use in source and binary forms, with or without        **
+** modification, are permitted provided that the following conditions        **
+** are met:                                                                  **
+** 1. Redistributions of source code must retain the above copyright         **
+**    notice, this list of conditions and the following disclaimer.          **
+** 2. Redistributions in binary form must reproduce the above copyright      **
+**    notice, this list of conditions and the following disclaimer in the    **
+**    documentation and/or other materials provided with the distribution.   **
+** 3. Neither the name of the copyright holder nor the names of its          **
+**    contributors may be used to endorse or promote products derived        **
+**    from this software without specific prior written permission.          **
+**                                                                           **
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS       **
+** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT         **
+** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR     **
+** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT      **
+** HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,    **
+** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED  **
+** TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR    **
+** PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF    **
+** LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING      **
+** NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS        **
+** SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.              **
+******************************************************************************/
+#ifndef LIBXS_SOURCE_H
+#define LIBXS_SOURCE_H
+
+#if defined(LIBXS_MACROS_H)
+# error Please do not include any LIBXS header other than libxs_source.h!
+#endif
+#if defined(LIBXS_BUILD)
+# error LIBXS_BUILD cannot be defined for the header-only LIBXS!
+#endif
+#if !defined(LIBXS_API)
+# define LIBXS_API LIBXS_EXTERN_C LIBXS_INLINE LIBXS_RETARGETABLE
+#endif
+
+/**
+ * This header is intentionally called "libxs_source.h" since the followings block
+ * includes *internal* files, and thereby exposes LIBXS's implementation.
+ * The so-called "header-only" usage model gives up the clearly defined binary interface
+ * (including support for hot-fixes after deployment), and requires to rebuild client
+ * code for every (internal) change of LIBXS. Please make sure to only rely on the
+ * public interface as the internal implementation may change without notice.
+ */
+#if defined(LIBXS_OFFLOAD_TARGET)
+# pragma offload_attribute(push,target(LIBXS_OFFLOAD_TARGET))
+#endif
+#include "../src/generator_common.c"
+#include "../src/generator_gemm.c"
+#include "../src/generator_gemm_avx2_microkernel.c"
+#include "../src/generator_gemm_avx512_fsdbcst.c"
+#include "../src/generator_gemm_avx512_microkernel_fsdbcst.c"
+#include "../src/generator_gemm_avx512_microkernel_nofsdbcst.c"
+#include "../src/generator_gemm_avx_microkernel.c"
+#include "../src/generator_gemm_common.c"
+#include "../src/generator_gemm_noarch.c"
+#include "../src/generator_gemm_sse3_avx_avx2_avx512.c"
+#include "../src/generator_gemm_sse3_microkernel.c"
+#include "../src/generator_matcopy.c"
+#include "../src/generator_matcopy_avx_avx512.c"
+#include "../src/generator_packed.c"
+#include "../src/generator_packed_gemm_ac_rm_avx_avx2_avx512.c"
+#include "../src/generator_packed_gemm_avx_avx512.c"
+#include "../src/generator_packed_gemm_bc_rm_avx_avx2_avx512.c"
+#include "../src/generator_packed_getrf_avx_avx512.c"
+#include "../src/generator_packed_trmm_avx_avx512.c"
+#include "../src/generator_packed_trsm_avx_avx512.c"
+#include "../src/generator_spgemm.c"
+#include "../src/generator_spgemm_csc_asparse.c"
+#include "../src/generator_spgemm_csc_bsparse.c"
+#include "../src/generator_spgemm_csc_bsparse_soa.c"
+#include "../src/generator_spgemm_csc_reader.c"
+#include "../src/generator_spgemm_csr_asparse.c"
+#include "../src/generator_spgemm_csr_asparse_reg.c"
+#include "../src/generator_spgemm_csr_asparse_soa.c"
+#include "../src/generator_spgemm_csr_bsparse_soa.c"
+#include "../src/generator_spgemm_csr_reader.c"
+#include "../src/generator_transpose.c"
+#include "../src/generator_transpose_avx_avx512.c"
+#include "../src/generator_x86_instructions.c"
+#include "../src/libxs_blocked_gemm.c"
+#include "../src/libxs_cpuid_x86.c"
+#include "../src/libxs_dnn.c"
+#include "../src/libxs_dnn_convolution.c"
+#include "../src/libxs_dnn_convolution_backward.c"
+#include "../src/libxs_dnn_convolution_forward.c"
+#include "../src/libxs_dnn_convolution_weight_update.c"
+#include "../src/libxs_dnn_elementwise.c"
+#include "../src/libxs_dnn_fullyconnected.c"
+#include "../src/libxs_dnn_fullyconnected_backward.c"
+#include "../src/libxs_dnn_fullyconnected_forward.c"
+#include "../src/libxs_dnn_fullyconnected_weight_update.c"
+#include "../src/libxs_dnn_fusedbatchnorm.c"
+#include "../src/libxs_dnn_fusedbatchnorm_backward.c"
+#include "../src/libxs_dnn_fusedbatchnorm_forward.c"
+#include "../src/libxs_dnn_pooling.c"
+#include "../src/libxs_dnn_pooling_backward.c"
+#include "../src/libxs_dnn_pooling_forward.c"
+#include "../src/libxs_dnn_rnncell.c"
+#include "../src/libxs_dnn_rnncell_backward_weight_update.c"
+#include "../src/libxs_dnn_rnncell_forward.c"
+#include "../src/libxs_dnn_tensor.c"
+#include "../src/libxs_ext.c"
+#include "../src/libxs_ext_blocked_gemm.c"
+#include "../src/libxs_ext_gemm.c"
+#include "../src/libxs_ext_xcopy.c"
+#include "../src/libxs_fsspmdm.c"
+#include "../src/libxs_gemm.c"
+#include "../src/libxs_generator.c"
+#include "../src/libxs_hash.c"
+#include "../src/libxs_main.c"
+#include "../src/libxs_malloc.c"
+#include "../src/libxs_math.c"
+#include "../src/libxs_mem.c"
+#include "../src/libxs_mhd.c"
+#include "../src/libxs_perf.c"
+#include "../src/libxs_python.c"
+#include "../src/libxs_rng.c"
+#include "../src/libxs_spmdm.c"
+#include "../src/libxs_sync.c"
+#include "../src/libxs_timer.c"
+#include "../src/libxs_trace.c"
+#include "../src/libxs_xcopy.c"
+#if defined(LIBXS_OFFLOAD_TARGET)
+# pragma offload_attribute(pop)
+#endif
+
+#endif /*LIBXS_SOURCE_H*/
