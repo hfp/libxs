a14de0fa2093a57f670f30573dadae9311930dc8
Hans Pabst <hans.pabst@intel.com>
2020-02-04 08:48:22 +0100

    Code cleanup.

--- src/libxs_main.c
+++ src/libxs_main.c
@@ -465,12 +465,7 @@ LIBXS_API_INTERN void internal_finalize(void)
       libxs_scratch_info scratch_info; size_t size_scratch = 0, size_private = 0;
       unsigned int linebreak = (0 == internal_print_statistic(stderr, target_arch, 1/*SP*/, 1, 0)) ? 1 : 0;
       if (0 == internal_print_statistic(stderr, target_arch, 0/*DP*/, linebreak, 0) && 0 != linebreak && NULL != target_arch) {
-        if (0 == libxs_se) {
-          fprintf(stderr, "\nLIBXS_TARGET: %s\n", target_arch);
-        }
-        else {
-          fprintf(stderr, "\nLIBXS_TARGET: %s*\n", target_arch);
-        }
+        fprintf(stderr, "\nLIBXS_TARGET: %s\n", target_arch);
       }
       if (EXIT_SUCCESS == libxs_get_scratch_info(&scratch_info)) {
         size_private = scratch_info.internal;
