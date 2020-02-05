27c4a5389b7b5d025b58a2627dbd9e411f2e6efa
Hans Pabst <hans.pabst@intel.com>
2020-02-05 08:23:41 +0100

    Removed duplicated definition (already in libxs_macros.h).

--- include/libxs_sync.h
+++ include/libxs_sync.h
@@ -461,9 +461,6 @@ typedef enum libxs_atomic_kind {
 #   endif
 #   define LIBXS_SYNC_YIELD YieldProcessor
 # else
-#   if !defined(_GNU_SOURCE) /* pthread_yield */
-#     define _GNU_SOURCE
-#   endif
 #   include <pthread.h>
 #   if defined(__APPLE__) && defined(__MACH__)
 #     define LIBXS_PTHREAD_FN(FN) LIBXS_CONCATENATE(FN, _np)
