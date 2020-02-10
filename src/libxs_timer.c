bf8504665ee825270f251621332d409bb7f2c02f
Hans Pabst <hans.pabst@intel.com>
2020-02-10 11:34:42 +0100

    Fixed linker error. Updated ABI specification.

--- src/libxs_timer.c
+++ src/libxs_timer.c
@@ -49,7 +49,8 @@
 #endif
 
 
-LIBXS_API_INTERN double libxs_timer_duration_rtc(libxs_timer_tickint tick0, libxs_timer_tickint tick1)
+/* public symbol but no prototype exposed */
+LIBXS_API double libxs_timer_duration_rtc(libxs_timer_tickint tick0, libxs_timer_tickint tick1)
 {
   double result = (double)LIBXS_DELTA(tick0, tick1);
 #if defined(_WIN32)
@@ -69,7 +70,8 @@ LIBXS_API_INTERN double libxs_timer_duration_rtc(libxs_timer_tickint tick0, libx
 }
 
 
-LIBXS_API_INTERN libxs_timer_tickint libxs_timer_tick_rtc(void)
+/* public symbol but no prototype exposed */
+LIBXS_API libxs_timer_tickint libxs_timer_tick_rtc(void)
 {
   libxs_timer_tickint result;
 #if defined(_WIN32)
