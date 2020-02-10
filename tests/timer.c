966e34b7aee4416256bc08ee64ceb7c45b6d486a
Hans Pabst <hans.pabst@intel.com>
2020-02-10 10:58:34 +0100

    Improved test case (timer).

--- tests/timer.c
+++ tests/timer.c
@@ -32,6 +32,10 @@
 #endif
 
 
+libxs_timer_tickint libxs_timer_tick_rtc(void);
+double libxs_timer_duration_rtc(libxs_timer_tickint, libxs_timer_tickint);
+
+
 LIBXS_INLINE int timer_sleep(unsigned int seconds)
 {
   int result;
@@ -50,25 +54,36 @@ int main(int argc, char* argv[])
   const int max_nseconds_input = (1 < argc ? atoi(argv[1]) : MAX_NSECONDS);
   const unsigned int max_nseconds = (unsigned int)LIBXS_UP2POT(LIBXS_MAX(max_nseconds_input, 1));
   const char *const env_delta = getenv("TIMER_DELTA");
-  const int max_delta = (NULL == env_delta || 0 == *env_delta) ? 0 : atoi(env_delta);
+  const int max_delta = (NULL == env_delta || 0 == *env_delta)
+#if defined(_DEBUG)
+    ? -1
+#else
+    ? 0
+#endif
+    : atoi(env_delta);
+  libxs_timer_tickint begin, start, rbegin, rend;
+  unsigned int n = max_nseconds, ninterrupts = 0;
   double total = 0, delta = 0, d, t;
-  unsigned int n = max_nseconds;
-  libxs_timer_tickint start;
   int result;
 
 #if !defined(USE_NOINIT)
   libxs_init();
 #endif
 
+  rbegin = libxs_timer_tick_rtc();
+  start = begin = libxs_timer_tick();
   for (n >>= 1; 0 < n; n >>= 1) {
-    start = libxs_timer_tick();
     if (EXIT_SUCCESS == timer_sleep(n)) {
       t = libxs_timer_duration(start, libxs_timer_tick());
       d = LIBXS_DELTA(t, (double)n);
       if (delta < d) delta = d;
       total += t;
     }
-    else total += (double)n;
+    else {
+      total += (double)n;
+      ++ninterrupts;
+    }
+    start = libxs_timer_tick();
   }
 
   start = libxs_timer_tick();
@@ -78,17 +93,28 @@ int main(int argc, char* argv[])
     if (delta < d) delta = d;
     total += t;
   }
-  else total += 1.0;
+  else {
+    ++ninterrupts;
+    total += 1.0;
+  }
+  start = libxs_timer_tick();
+  rend = libxs_timer_tick_rtc();
 
   d = LIBXS_DELTA(total, (double)max_nseconds);
   if (delta < d) delta = d;
 
   result = (int)LIBXS_ROUND(100.0 * delta);
-  if ((MAX_TOLPERC) >= result) {
+  if (0 > max_delta || result <= max_delta) {
+    const double r = libxs_timer_duration_rtc(rbegin, rend);
+    libxs_cpuid_x86_info info;
+    libxs_cpuid_x86(&info);
+    d = libxs_timer_duration(begin, start);
+    fprintf(stderr, "seconds=%f..%f delta=%s%i%% interrupted=%u tsc=%sconstant\n",
+      r, d, 0 == result ? "" : (total <= d ? "+" : "-"), result, ninterrupts,
+      info.constant_tsc ? "" : "non-");
     result = EXIT_SUCCESS;
   }
-  else if (0 > max_delta || result <= max_delta) {
-    fprintf(stderr, "delta=%i%%\n", result);
+  else if ((MAX_TOLPERC) >= result) {
     result = EXIT_SUCCESS;
   }
 
