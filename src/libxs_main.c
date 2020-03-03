a8def1e81969815b99c63d94256baf76bc657412
Hans Pabst <hans.pabst@intel.com>
2020-03-03 12:42:54 +0100

    Added some debug output [skip ci].

--- src/libxs_main.c
+++ src/libxs_main.c
@@ -522,7 +522,11 @@ LIBXS_API_INTERN void internal_finalize(void)
         }
       }
       if (LIBXS_VERBOSITY_HIGH < libxs_verbosity || 0 > libxs_verbosity) {
-        fprintf(stderr, "Uptime: %f s\n", libxs_timer_duration(internal_timer_start, libxs_timer_tick()));
+        fprintf(stderr, "Uptime: %f s", libxs_timer_duration(internal_timer_start, libxs_timer_tick()));
+        if (1 < libxs_thread_count && INT_MAX == libxs_verbosity) {
+          fprintf(stderr, " (nthreads=%u)", libxs_thread_count);
+        }
+        fprintf(stderr, "\n");
       }
     }
     else {
