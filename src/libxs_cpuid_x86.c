c48292a11de4c2a7f169959fa406db14e7cf520b
Hans Pabst <hans.pabst@intel.com>
2020-06-16 19:09:17 +0200

    Disabled opportunistic code path.

--- src/libxs_cpuid_x86.c
+++ src/libxs_cpuid_x86.c
@@ -171,7 +171,7 @@ LIBXS_API int libxs_cpuid_x86(libxs_cpuid_x86_info* info)
       /* macOS is faulting AVX-512 (on-demand larger state) */
       result = feature_cpu;
 # if !defined(__APPLE__) || !defined(__MACH__)
-#   if 1 /* opportunistic */
+#   if 0 /* opportunistic */
       if (0 == libxs_se)
 #   endif
       { /* only permitted features */
