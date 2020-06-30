b170fcc1a20f8dd5e0199ce7983c3c0e71ef5495
Hans Pabst <hans.pabst@intel.com>
2020-06-30 14:16:46 -0700

    enabled JIT transpose for f32 convolutions, removed #ifdef

--- src/libxs_dnn_convolution.c
+++ src/libxs_dnn_convolution.c
@@ -1410,12 +1410,7 @@ LIBXS_API_INLINE libxs_dnn_err_t libxs_dnn_convolution_setup( libxs_dnn_layer* h
 
   /* Transpose kernel used for filter transpose in bwd pass  */
   tr_desc = libxs_trans_descriptor_init(&blob, sizeof(float), 64, 16, 64);
-#if 1
   handle->tr_kernel = libxs_dispatch_trans(tr_desc);
-#else
-  handle->tr_kernel = NULL;
-  LIBXS_UNUSED( tr_desc );
-#endif
 
   /* UPD parameter setup */
   handle->upd_linearized_tasklist = libxs_dnn_convolution_setup_linearized_tasklist_upd(handle);
