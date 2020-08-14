b787589f60f7dc7390517952b33af3a2ad9b570e
Hans Pabst <hans.pabst@intel.com>
2020-08-14 13:18:50 +0200

    Minor extension of test case (malloc).

--- tests/malloc.c
+++ tests/malloc.c
@@ -15,14 +15,21 @@
 #if !defined(CHECK_REALLOC) && 1
 # define CHECK_REALLOC
 #endif
+#if !defined(CHECK_SCRATCH) && 1
+# define CHECK_SCRATCH
+#endif
 
 
 int main(void)
 {
-  const size_t size = 2507, alignment = (2U << 20);
+  const size_t size_malloc = 2507, alignment = (2U << 20);
   libxs_malloc_info malloc_info;
   int nerrors = 0;
-  void* p;
+  void *p;
+#if defined(CHECK_SCRATCH)
+  const size_t size_scratch = (24U << 20);
+  void *q, *r;
+#endif
 
 #if defined(CHECK_SETUP)
   { /* check allocator setup */
@@ -43,56 +50,67 @@ int main(void)
 #endif
 
   /* allocate some amount of memory */
-  p = libxs_malloc(size);
+  p = libxs_malloc(size_malloc);
 
   /* query and check the size of the buffer */
-  if (NULL != p && (EXIT_SUCCESS != libxs_get_malloc_info(p, &malloc_info) || malloc_info.size < size)) {
+  if (NULL != p && (EXIT_SUCCESS != libxs_get_malloc_info(p, &malloc_info) || malloc_info.size < size_malloc)) {
     ++nerrors;
   }
+
+#if defined(CHECK_SCRATCH)
+  q = libxs_aligned_scratch(size_scratch, 0/*auto*/);
+  libxs_free(q);
+  q = libxs_aligned_scratch(size_scratch / 3, 0/*auto*/);
+  r = libxs_aligned_scratch(size_scratch / 3, 0/*auto*/);
+  /* confirm malloc-info fails for an in-scratch buffer */
+  if (NULL != r && EXIT_SUCCESS == libxs_get_malloc_info(r, &malloc_info)) {
+    ++nerrors;
+  }
+#endif
 
 #if defined(CHECK_REALLOC)
   if (NULL != p) { /* reallocate larger amount of memory */
     const int palign = 1 << LIBXS_INTRINSICS_BITSCANFWD64((uintptr_t)p);
     unsigned char* c = (unsigned char*)p;
     size_t i;
-    for (i = 0; i < size; ++i) c[i] = (unsigned char)LIBXS_MOD2(i, 256);
-    p = libxs_realloc(size * 2, p);
+    for (i = 0; i < size_malloc; ++i) c[i] = (unsigned char)LIBXS_MOD2(i, 256);
+    p = libxs_realloc(size_malloc * 2, p);
     /* check that alignment is preserved */
     if (0 != (((uintptr_t)p) % palign)) {
       ++nerrors;
     }
     c = (unsigned char*)p;
-    for (i = size; i < (size * 2); ++i) c[i] = (unsigned char)LIBXS_MOD2(i, 256);
+    for (i = size_malloc; i < (size_malloc * 2); ++i) c[i] = (unsigned char)LIBXS_MOD2(i, 256);
     /* reallocate again with same size */
-    p = libxs_realloc(size * 2, p);
+    p = libxs_realloc(size_malloc * 2, p);
     /* check that alignment is preserved */
     if (0 != (((uintptr_t)p) % palign)) {
       ++nerrors;
     }
     c = (unsigned char*)p;
-    for (i = 0; i < (size * 2); ++i) { /* check that content is preserved */
+    for (i = 0; i < (size_malloc * 2); ++i) { /* check that content is preserved */
       nerrors += (c[i] == (unsigned char)LIBXS_MOD2(i, 256) ? 0 : 1);
     }
     /* reallocate with smaller size */
-    p = libxs_realloc(size / 2, p);
+    p = libxs_realloc(size_malloc / 2, p);
     /* check that alignment is preserved */
     if (0 != (((uintptr_t)p) % palign)) {
       ++nerrors;
     }
     c = (unsigned char*)p;
-    for (i = 0; i < size / 2; ++i) { /* check that content is preserved */
+    for (i = 0; i < size_malloc / 2; ++i) { /* check that content is preserved */
       nerrors += (c[i] == (unsigned char)LIBXS_MOD2(i, 256) ? 0 : 1);
     }
   }
-  /* query and check the size of the buffer */
-  if (NULL != p && (EXIT_SUCCESS != libxs_get_malloc_info(p, &malloc_info) || malloc_info.size < (size / 2))) {
+  /* query and check the size_malloc of the buffer */
+  if (NULL != p && (EXIT_SUCCESS != libxs_get_malloc_info(p, &malloc_info) || malloc_info.size < (size_malloc / 2))) {
     ++nerrors;
   }
   libxs_free(p); /* release buffer */
   /* check degenerated reallocation */
-  p = libxs_realloc(size, NULL/*allocation*/);
+  p = libxs_realloc(size_malloc, NULL/*allocation*/);
   /* query and check the size of the buffer */
-  if (NULL != p && (EXIT_SUCCESS != libxs_get_malloc_info(p, &malloc_info) || malloc_info.size < size)) {
+  if (NULL != p && (EXIT_SUCCESS != libxs_get_malloc_info(p, &malloc_info) || malloc_info.size < size_malloc)) {
     ++nerrors;
   }
 #endif
@@ -109,18 +127,22 @@ int main(void)
   libxs_free(p);
 
   /* allocate memory with specific alignment */
-  p = libxs_aligned_malloc(size, alignment);
+  p = libxs_aligned_malloc(size_malloc, alignment);
 
   /* check the alignment of the allocation */
   if (0 != (((uintptr_t)p) % alignment)) {
     ++nerrors;
   }
 
-  /* release aligned memory */
+  /* release memory */
   libxs_free(p);
+#if defined(CHECK_SCRATCH)
+  libxs_free(q);
+  libxs_free(r);
+#endif
 
   /* check foreign memory */
-  if (EXIT_SUCCESS == libxs_get_malloc_info(&size/*faulty pointer*/, &malloc_info)) {
+  if (EXIT_SUCCESS == libxs_get_malloc_info(&size_malloc/*faulty pointer*/, &malloc_info)) {
     ++nerrors;
   }
 
