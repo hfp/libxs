1d032a159bb83f87f496bab9371625e360bf2a3a
Hans Pabst <hans.pabst@intel.com>
2020-09-30 11:06:50 +0200

    Added script that helps to enable debug mode (Travis CI).

---
+++ scripts/tool_debugci.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+###############################################################################
+# Copyright (c) Intel Corporation - All rights reserved.                      #
+# This file is part of the LIBXS library.                                     #
+#                                                                             #
+# For information on the license, see the LICENSE file.                       #
+# Further information: https://github.com/hfp/libxs/                              #
+# SPDX-License-Identifier: BSD-3-Clause                                       #
+###############################################################################
+
+CURL=$(command -v curl)
+DOMAIN=org
+JOBID=$1
+TOKEN=$2
+
+if [ ! "${CURL}" ]; then
+  echo "Error: missing prerequisites!"
+  exit 1
+fi
+
+if [ ! "${JOBID}" ] || [ ! "${TOKEN}" ]; then
+  echo "Usage: $0 <jobid> <token>"
+  echo "       jobid: not the build-id; expand \"Build system information\" (job's log)"
+  echo "       token: API authentication (https://travis-ci.org/account/preferences)"
+  echo "See also https://docs.travis-ci.com/user/running-build-in-debug-mode/"
+  exit 1
+fi
+
+${CURL} -s -X POST \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/json" \
+  -H "Travis-API-Version: 3" \
+  -H "Authorization: token ${TOKEN}" \
+  -d "{\"quiet\": true}" \
+  https://api.travis-ci.${DOMAIN}/job/${JOBID}/debug
+
+RESULT=$?
+if [ "0" != "${RESULT}" ]; then
+  echo "Error (code ${RESULT}): failed to enable debug mode!"
+  exit ${RESULT}
+fi
+
