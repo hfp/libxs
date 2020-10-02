e740b484d22b1b6a749a2b6c1cc9de0b867e9e98
Hans Pabst <hans.pabst@intel.com>
2020-10-02 14:47:25 +0200

    Refined 1a71345729c1e30d9617a786d9072bd08e681695 in an attempt to fix compilation error with legacy Intel Compiler.

--- include/libxs_frontend.h
+++ include/libxs_frontend.h
@@ -56,8 +56,14 @@
 # endif
 #endif
 /** INTEL_MKL_VERSION is needed later to fix some NOTHROW issue. */
-#if defined(__MKL) && !defined(INTEL_MKL_VERSION)
-# include <mkl_version.h>
+#if defined(__MKL) && !defined(INTEL_MKL_VERSION) && defined(NOTHROW)
+# if defined(LIBXS_OFFLOAD_BUILD)
+#   pragma offload_attribute(push,target(LIBXS_OFFLOAD_TARGET))
+#   include <mkl_version.h>
+#   pragma offload_attribute(pop)
+# else
+#   include <mkl_version.h>
+# endif
 #endif
 
 /** Automatically select a prefetch-strategy (libxs_get_gemm_xprefetch, etc.). */
