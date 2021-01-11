35bf4c7e38b1874c36eea549141065a1f9897030
Hans Pabst <hans.pabst@intel.com>
2021-01-11 16:51:16 +0100

    Fixed a typo.

--- src/libxs_math.c
+++ src/libxs_math.c
@@ -140,7 +140,7 @@ LIBXS_API int libxs_matdiff(libxs_matdiff_info* info,
         info->var_ref = 0;
         info->l1_tst = info->l1_ref;
         info->l1_ref = 0;
-        info->v_tst = input->v_ref;
+        info->v_tst = info->v_ref;
         info->v_tst = 0;
       }
     }
