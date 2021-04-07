414468536894316bc3976c5ef7a10bf05b6e5e44
Hans Pabst <hans.pabst@intel.com>
2021-04-07 17:10:04 +0200

    Introduced LIBXS_AARCH64_V82, and runtime-detect LIBXS_AARCH64_V82 or LIBXS_AARCH64_A64FX (on top of LIBXS_AARCH64_V81 baseline). CPUID/runtime detection will be further improved/ported/tested on macOS, FreeBSD, and Windows.

--- src/libxs_cpuid_arm.c
+++ src/libxs_cpuid_arm.c
@@ -9,7 +9,16 @@
 #include <libxs_cpuid.h>
 #include <libxs_generator.h>
 #include <libxs_mem.h>
-#include <libxs_sync.h>
+
+#if defined(LIBXS_PLATFORM_AARCH64)
+# include "libxs_main.h"
+# include <asm/hwcap.h>
+# if (defined(LIBXS_BUILD) && (1 < (LIBXS_BUILD))) /* GLIBC */
+#   include <sys/auxv.h>
+# endif
+#else
+# include <libxs_sync.h>
+#endif
 
 
 LIBXS_API int libxs_cpuid_arm(libxs_cpuid_info* info)
@@ -28,8 +37,37 @@ LIBXS_API int libxs_cpuid_arm(libxs_cpuid_info* info)
 #else
   if (NULL != info) LIBXS_MEMZERO127(info);
 # if defined(LIBXS_PLATFORM_AARCH64)
-  /* @TODO add AARCH64 feature check */
   result = LIBXS_AARCH64_V81;
+  {
+#   if defined(LIBXS_INTERCEPT_DYNAMIC)
+    typedef unsigned long (*getcap_fn)(unsigned long);
+    static getcap_fn getcap = NULL;
+    if (NULL == getcap) {
+#     if defined(RTLD_DEFAULT)
+      void *const handle = RTLD_DEFAULT;
+#     else
+      void *const handle = dlopen(NULL, RTLD_LOCAL);
+#     endif
+      dlerror();
+      getcap = (getcap_fn)dlsym(handle, "getauxval");
+      if (NULL != dlerror()) getcap = NULL;
+#     if !defined(RTLD_DEFAULT)
+      if (NULL != handle) result = dlclose(handle);
+#     endif
+    }
+    if (NULL != getcap) {
+      const unsigned long capabilities = getcap(AT_HWCAP);
+#     if defined(HWCAP_DCPOP)
+      if (HWCAP_DCPOP & capabilities) result = LIBXS_AARCH64_V82;
+#     endif
+#     if defined(HWCAP_SVE)
+      if (HWCAP_SVE & capabilities) result = LIBXS_AARCH64_A64FX;
+#     endif
+    }
+    else {
+    }
+#   endif
+  }
 # endif
 #endif
   return result;
