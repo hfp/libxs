3189790fcece1cdc779af1b7ef016997accc1604
Hans Pabst <hans.pabst@intel.com>
2021-04-20 08:13:01 +0200

    Merge pull request #473 from FreddieWitherden/master
    Dynamically decide between sparse and dense kernels in dfsspmdm.

--- src/libxs_main.h
+++ src/libxs_main.h
@@ -825,7 +825,6 @@ struct LIBXS_RETARGETABLE libxs_dfsspmdm {
   int ldb;
   int ldc;
   int N_chunksize;
-  unsigned int* permute_operands;
   double* a_dense;
   libxs_dmmfunction kernel;
 };
@@ -837,7 +836,6 @@ struct LIBXS_RETARGETABLE libxs_sfsspmdm {
   int ldb;
   int ldc;
   int N_chunksize;
-  unsigned int* permute_operands;
   float* a_dense;
   libxs_smmfunction kernel;
 };
