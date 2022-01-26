6e74673711472da05e712fb2a3c4bbc0bf06734c
Hans Pabst <hans.pabst@intel.com>
2022-01-26 12:02:55 +0100

    Updated references of hfp/libxs (repository moved to libxs organization).

--- samples/utilities/sync/sync_barrier.c
+++ samples/utilities/sync/sync_barrier.c
@@ -3,7 +3,7 @@
 * This file is part of the LIBXS library.                                     *
 *                                                                             *
 * For information on the license, see the LICENSE file.                       *
-* Further information: https://github.com/hfp/libxs/                              *
+* Further information: https://github.com/hfp/libxs/                          *
 * SPDX-License-Identifier: BSD-3-Clause                                       *
 ******************************************************************************/
 #include <libxs_timer.h>
