1953d7c7b00ee13d7ebb7baac865795c923ff9a4
Hans Pabst <hans.pabst@intel.com>
2022-03-25 20:26:25 +0100

    Issue #574: complemented LIBXS_ACCESS with LIBXS_ACCESS_RAW
    * Provide LIBXS_ACCESS_RAW taking the typesize and returning an untyped address (void*); no const-correctness supported.
    * Moved somewhat special/awkward LIBXS_ACCESS_VALUE into private header (libxs_xcopy.h), and renamed it to LIBXS_VALUE1.

--- tests/vla.c
+++ tests/vla.c
@@ -35,14 +35,19 @@ int main(/*int argc, char* argv[]*/)
         const ELEM_TYPE gold0 = input[linear];
         const ELEM_TYPE test0a = VLA_IJK_INDX(3, jk3, i, j, k, nj, nk);
         const void *const vjk3 = LIBXS_CONCATENATE(jk3, LIBXS_VLA_POSTFIX);
-        const ELEM_TYPE test0b = *LIBXS_ACCESS(3, ELEM_TYPE, vjk3, i, j, k, nj, nk);
+        const void *const vpjk = LIBXS_ACCESS_RAW(3, sizeof(ELEM_TYPE), vjk3, i, j, k, nj, nk);
+        const ELEM_TYPE test0b = *(const ELEM_TYPE*)vpjk;
+        const ELEM_TYPE test0c = *LIBXS_ACCESS(3, ELEM_TYPE, vjk3, i, j, k, nj, nk);
         const ELEM_TYPE gold1 = VLA_IJK_INDX(3, kj3, i, k, j, nk, nj);
         const ELEM_TYPE test1a = VLA_IKJ_INDX(3, kj3, i, j, k, nj, nk);
         const void *const vkj3 = LIBXS_CONCATENATE(kj3, LIBXS_VLA_POSTFIX);
-        const ELEM_TYPE test1b = *LIBXS_ACCESS(3, ELEM_TYPE, vkj3, i, k, j, nk, nj);
+        const void *const vpkj = LIBXS_ACCESS_RAW(3, sizeof(ELEM_TYPE), vkj3, i, k, j, nk, nj);
+        const ELEM_TYPE test1b = *(const ELEM_TYPE*)vpkj;
+        const ELEM_TYPE test1c = *LIBXS_ACCESS(3, ELEM_TYPE, vkj3, i, k, j, nk, nj);
         if (gold0 != LIBXS_VLA_ACCESS(1, in1, linear) ||
             gold0 != test0a || gold1 != test1a ||
-            test0a != test0b || test1a != test1b)
+            test0a != test0b || test0b != test0c ||
+            test1a != test1b || test1b != test1c)
         {
           result = EXIT_FAILURE;
           j = nj; break;
