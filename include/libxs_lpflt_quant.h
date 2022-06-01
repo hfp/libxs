ca359ab0c7b3f49ccadbaa4b681c55bd29ab417f
Hans Pabst <hans.pabst@intel.com>
2022-06-01 15:47:35 +0200

    Updated VS projects and fixed portability issues.

--- include/libxs_lpflt_quant.h
+++ include/libxs_lpflt_quant.h
@@ -51,4 +51,4 @@ LIBXS_API void libxs_rne_convert_fp32_bf8(const float* in, libxs_bfloat8* out, u
 LIBXS_API void libxs_convert_bf8_f32(const libxs_bfloat8* in, float* out, unsigned int length);
 LIBXS_API void libxs_stochastic_convert_fp32_bf8(const float* in, libxs_bfloat8* out, unsigned int length);
 
-#endif /*LIBXS_LPFLT_QUNAT_H*/
+#endif /*LIBXS_LPFLT_QUANT_H*/
