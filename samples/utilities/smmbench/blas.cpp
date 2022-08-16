55f66c924c81eaf5d2503c0320c7b828a9d95702
Hans Pabst <hans.pabst@intel.com>
2022-08-16 07:09:05 +0200

    Introduced LIBXS_MKL_VERSION2.

--- samples/utilities/smmbench/blas.cpp
+++ samples/utilities/smmbench/blas.cpp
@@ -90,7 +90,7 @@ int main(int argc, char* argv[])
     const char *const env_check = getenv("CHECK");
     const int check = (NULL == env_check ? 0 : atoi(env_check));
 #elif (defined(__MKL) || defined(MKL_DIRECT_CALL_SEQ) || defined(MKL_DIRECT_CALL)) && \
-      (defined(LIBXS_MKL_VERSION3) && (LIBXS_VERSION3(11, 3, 0) <= LIBXS_MKL_VERSION3))
+      (defined(LIBXS_MKL_VERSION2) && (LIBXS_VERSION2(11, 3) <= LIBXS_MKL_VERSION2))
     /*const*/ int check = 1;
 #endif
 
@@ -155,7 +155,7 @@ int main(int argc, char* argv[])
       }
 
 #if (defined(__MKL) || defined(MKL_DIRECT_CALL_SEQ) || defined(MKL_DIRECT_CALL)) && \
-    (defined(LIBXS_MKL_VERSION3) && (LIBXS_VERSION3(11, 3, 0) <= LIBXS_MKL_VERSION3))
+    (defined(LIBXS_MKL_VERSION2) && (LIBXS_VERSION2(11, 3) <= LIBXS_MKL_VERSION2))
       std::vector<const ITYPE*> va_array(static_cast<size_t>(s)), vb_array(static_cast<size_t>(s));
       std::vector<OTYPE*> vc_array(static_cast<size_t>(s));
       const ITYPE* *const a_array = &va_array[0];
@@ -197,7 +197,7 @@ int main(int argc, char* argv[])
         fprintf(stdout, "\tduration: %.0f ms\n", 1000.0 * duration);
       } /* fallthrough */
 #if (defined(__MKL) || defined(MKL_DIRECT_CALL_SEQ) || defined(MKL_DIRECT_CALL)) && \
-    (defined(LIBXS_MKL_VERSION3) && (LIBXS_VERSION3(11, 3, 0) <= LIBXS_MKL_VERSION3))
+    (defined(LIBXS_MKL_VERSION2) && (LIBXS_VERSION2(11, 3) <= LIBXS_MKL_VERSION2))
       case 1: { // batched indirect
         fprintf(stdout, "Indirect (A,B,C)...\n");
         const unsigned long long start = libxs_timer_tick();
@@ -255,7 +255,7 @@ int main(int argc, char* argv[])
         fprintf(stdout, "\tduration: %.0f ms\n", 1000.0 * duration);
       } /* fallthrough */
 #if (defined(__MKL) || defined(MKL_DIRECT_CALL_SEQ) || defined(MKL_DIRECT_CALL)) && \
-    (defined(LIBXS_MKL_VERSION3) && (LIBXS_VERSION3(11, 3, 0) <= LIBXS_MKL_VERSION3))
+    (defined(LIBXS_MKL_VERSION2) && (LIBXS_VERSION2(11, 3) <= LIBXS_MKL_VERSION2))
       case 3: { // indirect A and C
         fprintf(stdout, "Indirect (A,C)...\n");
         for (libxs_blasint i = 0; i < s; ++i) {
@@ -318,7 +318,7 @@ int main(int argc, char* argv[])
         fprintf(stdout, "\tduration: %.0f ms\n", 1000.0 * duration);
       } /* fallthrough */
 #if (defined(__MKL) || defined(MKL_DIRECT_CALL_SEQ) || defined(MKL_DIRECT_CALL)) && \
-    (defined(LIBXS_MKL_VERSION3) && (LIBXS_VERSION3(11, 3, 0) <= LIBXS_MKL_VERSION3))
+    (defined(LIBXS_MKL_VERSION2) && (LIBXS_VERSION2(11, 3) <= LIBXS_MKL_VERSION2))
       case 5: { // indirect B and C
         fprintf(stdout, "Indirect (B,C)...\n");
         for (libxs_blasint i = 0; i < s; ++i) {
@@ -385,7 +385,7 @@ int main(int argc, char* argv[])
         fprintf(stdout, "\tduration: %.0f ms\n", 1000.0 * duration);
       } /* fallthrough */
 #if (defined(__MKL) || defined(MKL_DIRECT_CALL_SEQ) || defined(MKL_DIRECT_CALL)) && \
-    (defined(LIBXS_MKL_VERSION3) && (LIBXS_VERSION3(11, 3, 0) <= LIBXS_MKL_VERSION3))
+    (defined(LIBXS_MKL_VERSION2) && (LIBXS_VERSION2(11, 3) <= LIBXS_MKL_VERSION2))
       case 7: { // indirect A and B
         fprintf(stdout, "Indirect (A,B)...\n");
 #if defined(_OPENMP)
@@ -456,7 +456,7 @@ int main(int argc, char* argv[])
         fprintf(stdout, "\tduration: %.0f ms\n", 1000.0 * duration);
       } /* fallthrough */
 #if (defined(__MKL) || defined(MKL_DIRECT_CALL_SEQ) || defined(MKL_DIRECT_CALL)) && \
-    (defined(LIBXS_MKL_VERSION3) && (LIBXS_VERSION3(11, 3, 0) <= LIBXS_MKL_VERSION3))
+    (defined(LIBXS_MKL_VERSION2) && (LIBXS_VERSION2(11, 3) <= LIBXS_MKL_VERSION2))
       case 9: { // indirect cached
         fprintf(stdout, "Indirect cached...\n");
 #if defined(_OPENMP)
