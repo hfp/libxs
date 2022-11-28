bbb7fbdf97162eea242c6980ffc9c5b85f1e4089
Hans Pabst <hans.pabst@intel.com>
2022-11-28 14:20:05 +0100

    Fixed calling sysctl (FreeBSD and macOS).

--- src/libxs_cpuid_arm.c
+++ src/libxs_cpuid_arm.c
@@ -83,7 +83,8 @@ LIBXS_API_INTERN char libxs_cpuid_arm_vendor(char vendor[], size_t* vendor_size)
     if (NULL != vendor) {
 # if !defined(_WIN32) && !defined(__linux__)
       if (0 != sysctlbyname("machdep.cpu.brand_string", vendor, vendor_size, NULL, 0) || 0 == *vendor_size) {
-        if (0 != sysctl(HW_MODEL, vendor, vendor_size, NULL, 0) || 0 == *vendor_size) {
+        /*const*/ int mib[] = {CTL_HW, HW_MODEL};
+        if (0 != sysctl(mib, sizeof(mib) / sizeof(*mib), vendor, vendor_size, NULL, 0) || 0 == *vendor_size) {
           *vendor_size = 0;
           *vendor = '\0';
         }
