a4412f20df53519860c13342e8bfa096af99eb82
Hans Pabst <hans.pabst@intel.com>
2023-04-06 10:58:23 +0200

    Revised libxs_strimatch.

--- tests/memory.c
+++ tests/memory.c
@@ -40,18 +40,18 @@ int main(int argc, char* argv[])
   if (EXIT_SUCCESS == result && NULL != libxs_stristr(NULL, NULL)) result = EXIT_FAILURE;
 
   /* check libxs_strimatch */
-  if (EXIT_SUCCESS == result && 2 != libxs_strimatch("Co Product A", "Corp Prod B")) result = EXIT_FAILURE;
-  if (EXIT_SUCCESS == result && 2 != libxs_strimatch("Corp Prod B", "Co Product A")) result = EXIT_FAILURE;
-  if (EXIT_SUCCESS == result && 3 != libxs_strimatch("Co Product A", "Corp Prod AA")) result = EXIT_FAILURE;
-  if (EXIT_SUCCESS == result && 3 != libxs_strimatch("Corp Prod AA", "Co Product A")) result = EXIT_FAILURE;
-  if (EXIT_SUCCESS == result && 3 != libxs_strimatch("Corp Prod AA", "Co Product A")) result = EXIT_FAILURE;
-  if (EXIT_SUCCESS == result && 3 != libxs_strimatch("Co Product A", "Corp Prod AA")) result = EXIT_FAILURE;
-  if (EXIT_SUCCESS == result && 3 != libxs_strimatch("Corp Prod A", "Co Product A")) result = EXIT_FAILURE;
-  if (EXIT_SUCCESS == result && 3 != libxs_strimatch("Co Product A", "Corp Prod A")) result = EXIT_FAILURE;
-  if (EXIT_SUCCESS == result && 3 != libxs_strimatch("C Product A", "Cor Prod AA")) result = EXIT_FAILURE;
-  if (EXIT_SUCCESS == result && 3 != libxs_strimatch("Cor Prod AA", "C Product A")) result = EXIT_FAILURE;
-  if (EXIT_SUCCESS == result && 1 != libxs_strimatch("aaaa", "A A A A")) result = EXIT_FAILURE;
-  if (EXIT_SUCCESS == result && 1 != libxs_strimatch("A A A A", "aaaa")) result = EXIT_FAILURE;
+  if (EXIT_SUCCESS == result && 2 != libxs_strimatch("Co Product A", "Corp Prod B", NULL)) result = EXIT_FAILURE;
+  if (EXIT_SUCCESS == result && 2 != libxs_strimatch("Corp Prod B", "Co Product A", NULL)) result = EXIT_FAILURE;
+  if (EXIT_SUCCESS == result && 3 != libxs_strimatch("Co Product A", "Corp Prod AA", NULL)) result = EXIT_FAILURE;
+  if (EXIT_SUCCESS == result && 3 != libxs_strimatch("Corp Prod AA", "Co Product A", NULL)) result = EXIT_FAILURE;
+  if (EXIT_SUCCESS == result && 3 != libxs_strimatch("Corp Prod AA", "Co Product A", NULL)) result = EXIT_FAILURE;
+  if (EXIT_SUCCESS == result && 3 != libxs_strimatch("Co Product A", "Corp Prod AA", NULL)) result = EXIT_FAILURE;
+  if (EXIT_SUCCESS == result && 3 != libxs_strimatch("Corp Prod A", "Co Product A", NULL)) result = EXIT_FAILURE;
+  if (EXIT_SUCCESS == result && 3 != libxs_strimatch("Co Product A", "Corp Prod A", NULL)) result = EXIT_FAILURE;
+  if (EXIT_SUCCESS == result && 3 != libxs_strimatch("C Product A", "Cor Prod AA", NULL)) result = EXIT_FAILURE;
+  if (EXIT_SUCCESS == result && 3 != libxs_strimatch("Cor Prod AA", "C Product A", NULL)) result = EXIT_FAILURE;
+  if (EXIT_SUCCESS == result && 1 != libxs_strimatch("aaaa", "A A A A", NULL)) result = EXIT_FAILURE;
+  if (EXIT_SUCCESS == result && 1 != libxs_strimatch("A A A A", "aaaa", NULL)) result = EXIT_FAILURE;
 
   /* check LIBXS_MEMCPY127 and libxs_diff_n */
   if (EXIT_SUCCESS == result) {
