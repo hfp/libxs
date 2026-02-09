aa80999f95000f7bea2557eed7d3f60c075280a6
Hans Pabst <hans.pabst@intel.com>
2026-02-09 11:11:48 +0100

    Prepared wrapper sample code

---
+++ samples/wrap/wrap-gemm.c
@@ -0,0 +1,9 @@
+/******************************************************************************
+* Copyright (c) Intel Corporation - All rights reserved.                      *
+* This file is part of the LIBXS library.                                     *
+*                                                                             *
+* For information on the license, see the LICENSE file.                       *
+* Further information: https://github.com/hfp/libxs/                          *
+* SPDX-License-Identifier: BSD-3-Clause                                       *
+******************************************************************************/
+#include "gemm.h"
