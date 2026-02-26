60bd0b6dc4b5580b067c19f45df0d354256043e1
Hans Pabst <hans.pabst@intel.com>
2026-02-26 16:13:47 +0100

    Adjusted copyright banner
    - Banner unchanged for source code.
    - Simpler for scripts, etc

--- samples/ozaki/test-wrap.sh
+++ samples/ozaki/test-wrap.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 ###############################################################################
 # Copyright (c) Intel Corporation - All rights reserved.                      #
-# This file is part of the LIBXS library.                                     #
 #                                                                             #
 # For information on the license, see the LICENSE file.                       #
-# Further information: https://github.com/hfp/libxs/                          #
 # SPDX-License-Identifier: BSD-3-Clause                                       #
 ###############################################################################
 # shellcheck disable=SC2011
